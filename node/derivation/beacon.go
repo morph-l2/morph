ca709616d501b0093a64ec63a2f0e39614b29e9c
curryxbo <curryx1992@gmail.com>
2024-02-26 10:26:21 +0800

    fmt

--- node/derivation/beacon.go
+++ node/derivation/beacon.go
@@ -5,7 +5,6 @@ import (
 	"crypto/sha256"
 	"encoding/json"
 	"fmt"
-	"github.com/scroll-tech/go-ethereum/common/hexutil"
 	"io"
 	"net/http"
 	"net/url"
@@ -15,6 +14,7 @@ import (
 	"sync"
 
 	"github.com/scroll-tech/go-ethereum/common"
+	"github.com/scroll-tech/go-ethereum/common/hexutil"
 	"github.com/scroll-tech/go-ethereum/core/types"
 	"github.com/scroll-tech/go-ethereum/crypto/kzg4844"
 	"github.com/scroll-tech/go-ethereum/params"
