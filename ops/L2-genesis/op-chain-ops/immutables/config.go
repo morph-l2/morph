d0a2585739690d8627a9f4aa4697e56095dbda36
fletcher.fan <fletcher.fan@fletcher2fan.local>
2024-02-29 14:03:02 +0800

    init submitter at genesis stage

--- ops/L2-genesis/op-chain-ops/immutables/config.go
+++ ops/L2-genesis/op-chain-ops/immutables/config.go
@@ -2,10 +2,13 @@ package immutables
 
 import (
 	"github.com/scroll-tech/go-ethereum/common"
+	"github.com/scroll-tech/go-ethereum/common/hexutil"
 )
 
 type Config struct {
 	L2SequencerAddresses []common.Address `json:"l2SequencerAddresses"`
 	L2SequencerTmKeys    []common.Hash    `json:"l2SequencerTmKeys"`
 	L2SequencerBlsKeys   [][]byte         `json:"l2SequencerBlsKeys"`
+
+	L2GenesisBlockTimestamp hexutil.Uint64 `json:"l2GenesisBlockTimestamp"`
 }
