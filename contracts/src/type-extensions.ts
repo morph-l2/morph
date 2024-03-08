21e1fc16d4d000fc344eb3f6ee4ca6d84c5cfeba
kukoomomo <kukoomomo@gmail.com>
2024-03-08 12:18:49 +0800

    fix build

--- contracts/src/type-extensions.ts
+++ contracts/src/type-extensions.ts
@@ -1,6 +1,36 @@
 import 'hardhat/types/runtime'
 import 'hardhat/types/config'
 
+
+export type DeployConfigSpec<
+    TDeployConfig extends {
+        [key: string]: any
+    }
+> = {
+    [K in keyof TDeployConfig]: {
+        type: 'address' | 'number' | 'string' | 'boolean'
+        default?: any
+    }
+}
+
+declare module 'hardhat/types/config' {
+    interface HardhatUserConfig {
+        deployConfigSpec?: DeployConfigSpec<any>
+    }
+
+    interface HardhatConfig {
+        deployConfigSpec?: DeployConfigSpec<any>
+    }
+
+    interface ProjectPathsUserConfig {
+        deployConfig?: string
+    }
+
+    interface ProjectPathsConfig {
+        deployConfig?: string
+    }
+}
+
 declare module 'hardhat/types/runtime' {
     interface HardhatRuntimeEnvironment {
         deployConfig: {
