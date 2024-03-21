975a230b9e33bdb0a56274c1bdf464befed6431c
kukoomomo <kukoomomo@gmail.com>
2024-03-21 17:53:12 +0800

    fix slash only call by rollup

--- contracts/deploy/018-StakingInit.ts
+++ contracts/deploy/018-StakingInit.ts
@@ -106,6 +106,7 @@ export const StakingInit = async (
             StakingFactory.interface.encodeFunctionData('initialize', [
                 admin,
                 L1SequencerProxyAddress,
+                RollupProxyAddress,
                 sequencerSize,
                 limit,
                 hre.ethers.utils.parseEther(lock.toString()),
@@ -126,11 +127,16 @@ export const StakingInit = async (
             StakingFactory.interface,
             deployer,
         )
- 
+
         await assertContractVariable(
             contractTmp,
             'sequencerContract',
             L1SequencerProxyAddress
+        )
+        await assertContractVariable(
+            contractTmp,
+            'rollupContract',
+            RollupProxyAddress
         )
         await assertContractVariable(
             contractTmp,
