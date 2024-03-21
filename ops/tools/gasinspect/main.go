b01a03a0de36d5325c46af9cc7330fb3991280ee
fletcher.fan <fletcher.fan@fletcher2fan.local>
2024-03-21 14:49:06 +0800

    add tools

--- ops/tools/gasinspect/main.go
+++ ops/tools/gasinspect/main.go
@@ -6,200 +6,220 @@ import (
 	"encoding/binary"
 	"fmt"
 	"math/big"
-	"time"
 
 	"github.com/morph-l2/bindings/bindings"
 	"github.com/scroll-tech/go-ethereum/common"
-	eth "github.com/scroll-tech/go-ethereum/core/types"
+	"github.com/scroll-tech/go-ethereum/common/hexutil"
 	"github.com/scroll-tech/go-ethereum/ethclient"
-	"github.com/scroll-tech/go-ethereum/params"
-	"github.com/scroll-tech/go-ethereum/rlp"
-)
-
-var (
-	l1Url  = "https://eth-sepolia.g.alchemy.com/v2/3VpooM4yCL1TYnkaedcHraMo234vhN5y"
-	txHash = common.HexToHash("0x2fe371018e5c307eb1fe4b784158adcdd7741e27c18c43e723c8948b2583fde7")
+	"github.com/scroll-tech/go-ethereum/rollup/fees"
 )
 
 func main() {
-	client, err := ethclient.Dial(l1Url)
-	if err != nil {
-		panic(err)
-	}
+	// 0xb033f4a9c375ee95d7bda9ce462850d5b063bd0c66f841936c6b5b6ee5b80740
+	// 0xef0119b11c06eefe6e2db2770d36fdf535e30bee45acc44cc3f67314fc96afe7
+	// 0x38fa0d472a3774c180b99c3330292f54b576982c03862c51b2550a0b66a46c6b
+	// 0xcf36b911809af47fb00e68e87d1b62957893edb42a8e65b7c92e05edd781b5ff
+	// 0x862d1243ee2cbc5cdd8b938e161ca5853cf7304018306b7c30646b448641fdfc
+	// 0x354d9cfa06a124a44e5b9723cd84169a2573ebd6011a727ca2e37818ff28ec76
+	parseBatch(common.HexToHash("0x2e63e169ca6b26516c8fedfa6666de67ffba0a26772feda99771a212a181e674"))
 
-	rollupTxCostInfo(client, txHash)
-
-	//rollupTxCostInfo(client, common.HexToHash("0x25101c10431c31dc21304b8984b2d936f78ec53e0152bb6a1b6294aa7af941a4"))
-	//
-	//rollupTxCostInfo(client, common.HexToHash("0xf81c57f1aaf21026184c0869a61774d49e0d421f0dcdab0c1569d0709dabbd81"))
-	//
-	//rollupTxCostInfo(client, common.HexToHash("0x9e001e97ffd400ff1761065e84b358125f021daaa22082fd880ec0e46225a054"))
-	//
-	//rollupTxCostInfo(client, common.HexToHash("0x7be03a5c2a121cb133c10e34029762c80913fd09e0ea856aa4726b6926a52065"))
-	//
-	//rollupTxCostInfo(client, common.HexToHash("0xdd82c92dc80f55c76471a805f26f3f740dfccbe0179db3f539c4da0f3a7ea677"))
-	//
-	//rollupTxCostInfo(client, common.HexToHash("0xf5ab1ddc7d5aed9e016ad943475d0baffdbe6c2d94be1b951b419dbeca9edea8"))
-
+	// 0x104377bf038501f09f8a1ae1ed17bceae5ea781e371eedf057f0d624d449f342
+	// 0x2e40fd91306d2e60c569222bde27754c73ef7f9edcf1046ba23152020ea8ef36
+	// 0xc4a8d18102e1cea37bbf93a69b6e7141b23a6420b7ff0017a8492192f8718969
+	// 0x1b36e9727d159f13e818fa8543700d709d6e84d297240c9924a9a3106c161214
+	// 0x15286e0866642596595e3bbb625ce62ef444b8de7d2f2df840e9ad58df88ce8b
+	// 0x5a2de6e67f488ca43c36b38d8ae278162acdc16730c97e3ed00f9e000680d44a
+	// 0x4b5e99a8b75ff4d1f3e98cab9183d36e9eee9db28637eebc0567e5a4c65e986e
+	//parsePreBatch(common.HexToHash("0x4b5e99a8b75ff4d1f3e98cab9183d36e9eee9db28637eebc0567e5a4c65e986e"))
 }
 
-func rollupTxCostInfo(client *ethclient.Client, txHash common.Hash) {
-	tx, _, err := client.TransactionByHash(context.Background(), txHash)
+func parsePreBatch(txHash common.Hash) {
+	//client, err := ethclient.Dial("http://l2-qa-morph-l1-geth.bitkeep.tools")
+	client, err := ethclient.Dial("https://sepolia.infura.io/v3/193cbc4d8995453898757cd575ed37f9")
 	if err != nil {
 		panic(err)
 	}
-	abi, err := bindings.RollupMetaData.GetAbi()
-	if err != nil {
-		panic(err)
-	}
-	args, err := abi.Methods["submitBatches"].Inputs.Unpack(tx.Data()[4:])
-	if err != nil {
-		panic(err)
-	}
-
-	rollupBatchDatas := args[0].([]struct {
-		BlockNumber    uint64    "json:\"blockNumber\""
-		Transactions   []uint8   "json:\"transactions\""
-		BlockWitness   []uint8   "json:\"blockWitness\""
-		PreStateRoot   [32]uint8 "json:\"preStateRoot\""
-		PostStateRoot  [32]uint8 "json:\"postStateRoot\""
-		WithdrawalRoot [32]uint8 "json:\"withdrawalRoot\""
-		Signature      struct {
-			Signers   [][]uint8 "json:\"signers\""
-			Signature []uint8   "json:\"signature\""
-		} "json:\"signature\""
-	})
-
-	var (
-		blockNum         int
-		txNum            int
-		flexibleCost     uint64
-		witnessCost      uint64
-		transactionsCost uint64
-	)
-	for _, batch := range rollupBatchDatas {
-		thisWitnessC := dataGasCost(batch.BlockWitness)
-		thisTxC := dataGasCost(batch.Transactions)
-		witnessCost += thisWitnessC
-		transactionsCost += thisTxC
-		flexibleCost += thisWitnessC + thisTxC
-
-		blockContexts, err := DecodeBlockContext(batch.BlockWitness)
-		if err != nil {
-			panic(err)
-		}
-		blockNum += len(blockContexts)
-
-		txs, err := DecodeTransactions(batch.Transactions)
-		if err != nil {
-			panic(err)
-		}
-		txNum += len(txs)
-	}
-
 	receipt, err := client.TransactionReceipt(context.Background(), txHash)
 	if err != nil {
 		panic(err)
 	}
-	dataGas := dataGasCost(tx.Data())
-	blockHeader, err := client.HeaderByHash(context.Background(), receipt.BlockHash)
+	if receipt.Status != 1 {
+		panic("tx does not success")
+	}
+	tx, _, err := client.TransactionByHash(context.Background(), txHash)
 	if err != nil {
 		panic(err)
 	}
-	fmt.Printf("====>(%s)tx hash: %s \n	batchNum: %d \n	blockNum: %d \n	txNum: %d \n	gas used total: %d \n	calldata cost: %d \n	  -witnessCost: %d\n	  -transactionsCost: %d \n	  -otherFixedCost: %d \n	execution cost: %d \n	G: fixedCallData+execution cost: %d \n",
-		time.Unix(int64(blockHeader.Time), 0).String(),
-		txHash.Hex(),
-		len(rollupBatchDatas),
-		blockNum,
-		txNum,
-		receipt.GasUsed,
-		dataGas,
-		witnessCost,
-		transactionsCost,
-		dataGas-flexibleCost,
-		receipt.GasUsed-dataGas,
-		receipt.GasUsed-flexibleCost,
-	)
-	fmt.Println()
-}
 
-func dataGasCost(data []byte) uint64 {
-	zeroes, ones := zeroesAndOnes(data)
-	zeroesGas := zeroes * params.TxDataZeroGas
-	onesGas := (ones) * params.TxDataNonZeroGasEIP2028
-	return zeroesGas + onesGas
-}
-
-// zeroesAndOnes counts the number of 0 bytes and non 0 bytes in a byte slice
-func zeroesAndOnes(data []byte) (uint64, uint64) {
-	var zeroes uint64
-	var ones uint64
-	for _, byt := range data {
-		if byt == 0 {
-			zeroes++
-		} else {
-			ones++
-		}
+	rollupABI, err := bindings.RollupMetaData.GetAbi()
+	if err != nil {
+		panic(err)
 	}
-	return zeroes, ones
+
+	method := rollupABI.Methods["commitBatch"]
+	ret, err := method.Inputs.UnpackValues(tx.Data()[4:])
+	if err != nil {
+		panic(err)
+	}
+	batchDataInterface := ret[0]
+	bd := batchDataInterface.(struct {
+		Version                uint8     "json:\"version\""
+		ParentBatchHeader      []uint8   "json:\"parentBatchHeader\""
+		Chunks                 [][]byte  "json:\"chunks\""
+		SkippedL1MessageBitmap []uint8   "json:\"skippedL1MessageBitmap\""
+		PrevStateRoot          [32]uint8 "json:\"prevStateRoot\""
+		PostStateRoot          [32]uint8 "json:\"postStateRoot\""
+		WithdrawalRoot         [32]uint8 "json:\"withdrawalRoot\""
+		Signature              struct {
+			Version   *big.Int   "json:\"version\""
+			Signers   []*big.Int "json:\"signers\""
+			Signature []uint8    "json:\"signature\""
+		} "json:\"signature\""
+	})
+	chunks := bd.Chunks
+	fmt.Println("chunks length", len(chunks))
+	calldataGasUsed := fees.CalculateL1GasUsed(tx.Data(), big.NewInt(0))
+	totalBlockNum, totalL2TxNum, gasUsedForTxs, _ := parseChunks(chunks, true)
+
+	fmt.Printf("Batch details for txHash: %x \n  ===totalBlockNum: %d \n  ===totalL2TxNum: %d \n  ===totalGasUsed: %d \n  ===calldataGasUsed: %s \n ===gasUsedForTxs: %s \n",
+		txHash, totalBlockNum, totalL2TxNum, receipt.GasUsed, calldataGasUsed, gasUsedForTxs.String())
+
+	//r := new(big.Float).Quo(big.NewFloat(float64(receipt.GasUsed+156000)), big.NewFloat(1.15))
+	//r1, _ := r.Int64()
+
+	r1 := int64(receipt.GasUsed + 156000)
+
+	overhead := (r1 - gasUsedForTxs.Int64()) / int64(totalL2TxNum)
+	fmt.Println("overhead: ", overhead)
+
+	collectedGasFloat := new(big.Float).Mul(big.NewFloat(float64(gasUsedForTxs.Int64()+2500*int64(totalL2TxNum))), big.NewFloat(1.15))
+	collectedGas, _ := collectedGasFloat.Int64()
+	earnGas := collectedGas - int64(receipt.GasUsed+156000)
+	fmt.Println("earnGas: ", earnGas)
 }
 
-// number || timestamp || base_fee || gas_limit || num_txs || tx_hashs
-type BlockInfo struct {
-	Number    *big.Int
-	Timestamp uint64
-	BaseFee   *big.Int
-	GasLimit  uint64
-	NumTxs    uint64
+func parseBatch(txHash common.Hash) {
+	//client, err := ethclient.Dial("http://l2-qa-morph-l1-geth.bitkeep.tools")
+	client, err := ethclient.Dial("http://127.0.0.1:9545")
+	if err != nil {
+		panic(err)
+	}
+	receipt, err := client.TransactionReceipt(context.Background(), txHash)
+	if err != nil {
+		panic(err)
+	}
+	if receipt.Status != 1 {
+		panic("tx does not success")
+	}
+	tx, _, err := client.TransactionByHash(context.Background(), txHash)
+	if err != nil {
+		panic(err)
+	}
+
+	rollupABI, err := bindings.RollupMetaData.GetAbi()
+	if err != nil {
+		panic(err)
+	}
+
+	method := rollupABI.Methods["commitBatch"]
+	ret, err := method.Inputs.UnpackValues(tx.Data()[4:])
+	if err != nil {
+		panic(err)
+	}
+	batchDataInterface := ret[0]
+	bd := batchDataInterface.(struct {
+		Version                uint8     "json:\"version\""
+		ParentBatchHeader      []uint8   "json:\"parentBatchHeader\""
+		Chunks                 [][]byte  "json:\"chunks\""
+		SkippedL1MessageBitmap []uint8   "json:\"skippedL1MessageBitmap\""
+		PrevStateRoot          [32]uint8 "json:\"prevStateRoot\""
+		PostStateRoot          [32]uint8 "json:\"postStateRoot\""
+		WithdrawalRoot         [32]uint8 "json:\"withdrawalRoot\""
+		Signature              struct {
+			Version   *big.Int   "json:\"version\""
+			Signers   []*big.Int "json:\"signers\""
+			Signature []uint8    "json:\"signature\""
+		} "json:\"signature\""
+	})
+	chunks := bd.Chunks
+	fmt.Println("chunks length", len(chunks))
+	calldataGasUsed := fees.CalculateL1GasUsed(tx.Data(), big.NewInt(0))
+	totalBlockNum, totalL2TxNum, _, l2TxHashes := parseChunks(chunks, false)
+
+	l2client, err := ethclient.Dial("http://127.0.0.1:8545")
+	if err != nil {
+		panic(err)
+	}
+	var totalTxBytes []byte
+	for _, txHash := range l2TxHashes {
+		tx, _, err := l2client.TransactionByHash(context.Background(), txHash)
+		if err != nil {
+			panic(err)
+		}
+		txBytes, err := tx.MarshalBinary()
+		if err != nil {
+			panic(err)
+		}
+		fmt.Println("one tx cost: ", fees.CalculateL1GasUsed(txBytes, big.NewInt(2500)))
+		fmt.Println("one tx cost2: ", fees.CalculateL1GasUsed(txBytes, big.NewInt(0)))
+		fmt.Println()
+		totalTxBytes = append(totalTxBytes, txBytes...)
+	}
+	collectedTxGasUsed := fees.CalculateL1GasUsed(totalTxBytes, big.NewInt(0))
+	fmt.Printf("Batch details for txHash: %x \n  ===totalBlockNum: %d \n  ===totalL2TxNum: %d \n  ===totalGasUsed: %d \n  ===calldataGasUsed: %s \n  ===collectedTxGasUsed: %s \n \n",
+		txHash, totalBlockNum, totalL2TxNum, receipt.GasUsed, calldataGasUsed, collectedTxGasUsed.String())
+
+	//r := new(big.Float).Quo(big.NewFloat(float64(receipt.GasUsed+131072+156000)), big.NewFloat(1.15))
+	//r1, _ := r.Int64()
+
+	r1 := int64(receipt.GasUsed + 131072 + 156000)
+	overhead := (r1 - collectedTxGasUsed.Int64()) / int64(totalL2TxNum)
+	fmt.Println("overhead: ", overhead)
 }
 
-func DecodeBlockContext(bs []byte) ([]*BlockInfo, error) {
-	blockContexts := make([]*BlockInfo, 0)
-	// [block1, block2, ..., blockN]
-	reader := bytes.NewReader(bs)
-	for {
-		if reader.Len() == 0 {
-			break
+func parseChunks(chunks [][]byte, non4844 bool) (totalBlockNum uint, totalL2TxNum uint16, gasUsedForTxs *big.Int, l2TxHashes []common.Hash) {
+	var txBytes []byte
+	for i, chunk := range chunks {
+		// oriChunkBytes := make([]byte, 0)
+		// copy(oriChunkBytes, chunk)
+		fmt.Printf("chunk %d: %s \n", i, hexutil.Encode(chunk))
+		reader := bytes.NewReader(chunk)
+		var blockNum uint8
+		if err := binary.Read(reader, binary.BigEndian, &blockNum); err != nil {
+			panic(err)
 		}
-		block := new(BlockInfo)
-		// number || timestamp || base_fee || gas_limit || num_txs
-		bsBlockNumber := make([]byte, 32)
-		if _, err := reader.Read(bsBlockNumber[:]); err != nil {
-			return nil, err
-		}
-		block.Number = new(big.Int).SetBytes(bsBlockNumber)
-
-		if err := binary.Read(reader, binary.BigEndian, &block.Timestamp); err != nil {
-			return nil, err
-		}
-		// [32]byte uint256
-		bsBaseFee := make([]byte, 32)
-		if _, err := reader.Read(bsBaseFee[:]); err != nil {
-			return nil, err
-		}
-		block.BaseFee = new(big.Int).SetBytes(bsBaseFee)
-		if err := binary.Read(reader, binary.BigEndian, &block.GasLimit); err != nil {
-			return nil, err
-		}
-		if err := binary.Read(reader, binary.BigEndian, &block.NumTxs); err != nil {
-			return nil, err
-		}
-		for i := 0; i < int(block.NumTxs); i++ {
-			txHash := common.Hash{}
-			if _, err := reader.Read(txHash[:]); err != nil {
-				return nil, err
+		totalBlockNum += uint(blockNum)
+		for i := 0; i < int(blockNum); i++ {
+			bc := make([]byte, 60)
+			if err := binary.Read(reader, binary.BigEndian, &bc); err != nil {
+				panic(err)
 			}
-			// drop txHash
+			totalTxNum := binary.BigEndian.Uint16(bc[56:58])
+			l1TxNum := binary.BigEndian.Uint16(bc[58:])
+			l2TxNum := totalTxNum - l1TxNum
+			totalL2TxNum += l2TxNum
 		}
-		blockContexts = append(blockContexts, block)
+		txBytes = append(txBytes, chunk[1+60*uint(blockNum):]...)
 	}
-	return blockContexts, nil
-}
+	if non4844 {
+		fmt.Println("txBytes length: ", len(txBytes))
+		if len(txBytes) > 0 {
+			gasUsedForTxs = fees.CalculateL1GasUsed(txBytes, big.NewInt(0))
+		} else {
+			gasUsedForTxs = big.NewInt(0)
+		}
+	} else {
+		count := len(txBytes) / 32
+		fmt.Println("txHashes count: ", len(txBytes)/32)
+		reader := bytes.NewReader(txBytes)
+		for i := 0; i < count; i++ {
+			hashBz := make([]byte, 32)
+			if err := binary.Read(reader, binary.BigEndian, &hashBz); err != nil {
+				panic(err)
+			}
+			l2TxHashes = append(l2TxHashes, common.BytesToHash(hashBz))
+		}
+	}
 
-func DecodeTransactions(bs []byte) ([]*eth.Transaction, error) {
-	ret := make([]*eth.Transaction, 0)
-	if err := rlp.DecodeBytes(bs, &ret); err != nil {
-		return nil, err
-	}
-	return ret, nil
+	return
 }
---
+++ ops/tools/multisend/main.go
@@ -0,0 +1,117 @@
+package main
+
+import (
+	"context"
+	"crypto/ecdsa"
+	"errors"
+	"fmt"
+	"math/big"
+	"time"
+
+	"github.com/scroll-tech/go-ethereum"
+	"github.com/scroll-tech/go-ethereum/accounts/abi"
+	"github.com/scroll-tech/go-ethereum/accounts/abi/bind"
+	"github.com/scroll-tech/go-ethereum/common"
+	"github.com/scroll-tech/go-ethereum/common/hexutil"
+	"github.com/scroll-tech/go-ethereum/core/types"
+	"github.com/scroll-tech/go-ethereum/crypto"
+	"github.com/scroll-tech/go-ethereum/ethclient"
+)
+
+var (
+	fundPrivKey = crypto.ToECDSAUnsafe(hexutil.MustDecode("0x3e4bde571b86929bf08e2aaad9a6a1882664cd5e65b96fff7d03e1c4e6dfa15c"))
+	chainId     = big.NewInt(53077)
+	url         = "http://localhost:8545"
+	senderNum   = 3
+	duration    = 120 * time.Minute
+)
+
+func main() {
+	client, err := ethclient.Dial(url)
+	if err != nil {
+		panic(err)
+	}
+
+	senderPks := distribute(client)
+	receiver, _ := crypto.GenerateKey()
+	to := crypto.PubkeyToAddress(receiver.PublicKey)
+	start := time.Now()
+	var count int
+	for {
+		if count == 1000000 || time.Now().Sub(start) > duration {
+			fmt.Println("completed")
+			break
+		}
+		for _, sender := range senderPks {
+			go func(s *ecdsa.PrivateKey) {
+				transactOpts, err := bind.NewKeyedTransactorWithChainID(s, chainId)
+				if err != nil {
+					panic(err)
+				}
+				transactOpts.Value = big.NewInt(10)
+				_, err = Transfer(transactOpts, to, client)
+				if err != nil {
+					fmt.Printf("error found when transfer: %v \n", err)
+				}
+				fmt.Println("sent tx, count: ", count)
+			}(sender)
+		}
+		count++
+		time.Sleep(900 * time.Millisecond)
+	}
+}
+
+func distribute(client *ethclient.Client) []*ecdsa.PrivateKey {
+	fundAddr := crypto.PubkeyToAddress(fundPrivKey.PublicKey)
+	balance, err := client.BalanceAt(context.Background(), fundAddr, nil)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("fundBalance: ", balance.String())
+	senderPks := make([]*ecdsa.PrivateKey, senderNum)
+	distributeTxHashes := make([]common.Hash, senderNum)
+	for i := 0; i < senderNum; i++ {
+		senderPriKey, _ := crypto.GenerateKey()
+		to := crypto.PubkeyToAddress(senderPriKey.PublicKey)
+		transactOpts, err := bind.NewKeyedTransactorWithChainID(fundPrivKey, chainId)
+		if err != nil {
+			panic(err)
+		}
+		value, _ := new(big.Int).SetString("10000000000000000000", 10)
+		transactOpts.Value = value
+		tx, err := Transfer(transactOpts, to, client)
+		if err != nil {
+			panic(err)
+		}
+		senderPks[i] = senderPriKey
+		distributeTxHashes[i] = tx.Hash()
+	}
+
+	time.Sleep(2 * time.Second)
+
+	for _, txHash := range distributeTxHashes {
+		receipt, err := client.TransactionReceipt(context.Background(), txHash)
+		if err != nil {
+			if err == ethereum.NotFound {
+				time.Sleep(time.Second)
+				receipt, err = client.TransactionReceipt(context.Background(), txHash)
+				if err != nil {
+					panic(fmt.Errorf("still cannot find receipt after retry: %v", err))
+				}
+			} else {
+				panic(err)
+			}
+		}
+		if receipt.Status != 1 {
+			panic(errors.New("tx failed when distribution"))
+		}
+	}
+	fmt.Println("finished distribution")
+	return senderPks
+}
+
+func Transfer(opts *bind.TransactOpts, to common.Address, client *ethclient.Client) (*types.Transaction, error) {
+	bc := bind.NewBoundContract(to, abi.ABI{}, client, client, nil)
+	opts.GasLimit = 21000
+	return bc.Transfer(opts)
+}
