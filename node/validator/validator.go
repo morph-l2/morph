d689a494ca0fa0a432a99138c95f39b4139ceb81
curryxbo <curryx1992@gmail.com>
2024-04-22 12:15:45 +0800

    fmt

--- node/validator/validator.go
+++ node/validator/validator.go
@@ -5,7 +5,6 @@ import (
 	"crypto/ecdsa"
 	"errors"
 	"fmt"
-	"github.com/scroll-tech/go-ethereum/crypto"
 	"math/big"
 	"time"
 
@@ -13,6 +12,7 @@ import (
 	"github.com/scroll-tech/go-ethereum"
 	"github.com/scroll-tech/go-ethereum/accounts/abi/bind"
 	ethtypes "github.com/scroll-tech/go-ethereum/core/types"
+	"github.com/scroll-tech/go-ethereum/crypto"
 	"github.com/scroll-tech/go-ethereum/ethclient"
 	"github.com/scroll-tech/go-ethereum/log"
 	tmlog "github.com/tendermint/tendermint/libs/log"
