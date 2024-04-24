69fd2d10d9308f5c0f0abfbe8c2f3c33a5dd5947
curryxbo <curryx1992@gmail.com>
2024-04-24 20:22:42 +0800

    add oracle

---
+++ oracle/staking-oracle/batch.go
@@ -0,0 +1,175 @@
+package main
+
+import (
+	"context"
+	"errors"
+	"fmt"
+
+	"github.com/morph-l2/bindings/bindings"
+	"github.com/morph-l2/node/derivation"
+	"github.com/scroll-tech/go-ethereum"
+	"github.com/scroll-tech/go-ethereum/common"
+	"github.com/scroll-tech/go-ethereum/common/hexutil"
+	"github.com/scroll-tech/go-ethereum/core/types"
+	"github.com/scroll-tech/go-ethereum/crypto"
+	"github.com/scroll-tech/go-ethereum/eth"
+	"github.com/scroll-tech/go-ethereum/log"
+	"math/big"
+)
+
+var (
+	RollupEventTopic     = "CommitBatch(uint256,bytes32)"
+	RollupEventTopicHash = crypto.Keccak256Hash([]byte(RollupEventTopic))
+)
+
+type BatchInfoMap map[common.Hash][]BatchInfo
+type RollupBatch struct {
+	TxCount uint64
+}
+
+type BatchInfo struct {
+	BatchIndex    uint64
+	L1BlockNumber uint64
+	L1TxHash      common.Hash
+	L2BlockNumber uint64
+	L2BlockCount  uint64
+	L2TxCount     uint64
+}
+
+func (o *Oracle) GetBatchSubmission(ctx context.Context, startBlock, endBlock uint64) ([]bindings.IRecordBatchSubmission, error) {
+	rLogs, err := o.fetchRollupLog(ctx, startBlock, endBlock)
+	if err != nil {
+		return nil, err
+	}
+	var recordBatchSubmissiones []bindings.IRecordBatchSubmission
+	o.record.NextBatchSubmissionIndex(nil)
+	for _, lg := range rLogs {
+		tx, pending, err := o.l1Client.TransactionByHash(ctx, lg.TxHash)
+		if err != nil {
+			return nil, err
+		}
+		signer := types.NewLondonSigner(tx.ChainId())
+		msg, err := tx.AsMessage(signer, tx.GasFeeCap())
+		if err != nil {
+			return nil, err
+		}
+		header, err := o.l1Client.HeaderByNumber(context.Background(), big.NewInt(int64(lg.BlockNumber)))
+		if err != nil {
+			return nil, err
+		}
+		if pending {
+			return nil, errors.New("pending transaction")
+		}
+		abi, err := bindings.RollupMetaData.GetAbi()
+		if err != nil {
+			return nil, err
+		}
+		rollupCommitBatch, parseErr := o.rollup.ParseCommitBatch(lg)
+		if parseErr != nil {
+			log.Error("get l2 BlockNumber", "err", err)
+			return nil, parseErr
+		}
+		args, err := abi.Methods["commitBatch"].Inputs.Unpack(tx.Data()[4:])
+		if err != nil {
+			if rollupCommitBatch.BatchIndex.Uint64() == 0 {
+				continue
+			}
+			log.Error("fetch batch info failed", "txHash", lg.TxHash, "blockNumber", lg.BlockNumber, "error", err)
+			return nil, err
+		}
+		if err != nil {
+			return nil, fmt.Errorf("unpack commitBatch error:%v", err)
+		}
+		rollupBatchData := args[0].(struct {
+			Version                uint8     "json:\"version\""
+			ParentBatchHeader      []uint8   "json:\"parentBatchHeader\""
+			Chunks                 [][]uint8 "json:\"chunks\""
+			SkippedL1MessageBitmap []uint8   "json:\"skippedL1MessageBitmap\""
+			PrevStateRoot          [32]uint8 "json:\"prevStateRoot\""
+			PostStateRoot          [32]uint8 "json:\"postStateRoot\""
+			WithdrawalRoot         [32]uint8 "json:\"withdrawalRoot\""
+			Signature              struct {
+				Version   *big.Int   "json:\"version\""
+				Signers   []*big.Int "json:\"signers\""
+				Signature []uint8    "json:\"signature\""
+			} "json:\"signature\""
+		})
+
+		var chunks []hexutil.Bytes
+		for _, chunk := range rollupBatchData.Chunks {
+			chunks = append(chunks, chunk)
+		}
+		batch := eth.RPCRollupBatch{
+			Version:                uint(rollupBatchData.Version),
+			ParentBatchHeader:      rollupBatchData.ParentBatchHeader,
+			Chunks:                 chunks,
+			SkippedL1MessageBitmap: rollupBatchData.SkippedL1MessageBitmap,
+			PrevStateRoot:          common.BytesToHash(rollupBatchData.PrevStateRoot[:]),
+			PostStateRoot:          common.BytesToHash(rollupBatchData.PostStateRoot[:]),
+			WithdrawRoot:           common.BytesToHash(rollupBatchData.WithdrawalRoot[:]),
+		}
+		var batchData derivation.BatchInfo
+		if err = batchData.ParseBatch(batch); err != nil {
+			return nil, fmt.Errorf("parse batch error:%v", err)
+		}
+
+		//if rollupCommitBatch.BatchIndex.Uint64() != highestBatchIndex+1 {
+		//	return nil, fmt.Errorf("invalid batch,batch index discontinuity or batch storage blockNumber too small,bs.BlockNumber:%v,HighestBatch.BlockNumber:%v,bs.BatchIndex:%v,HighestBatch.BatchIndex:%v,txHash:%v", lg.BlockNumber, HighestBatch.BlockNumber, rollupCommitBatch.BatchIndex.Uint64(), HighestBatch.BatchIndex, lg.TxHash)
+		//}
+		log.Info("received new batch", "batch_index", rollupCommitBatch.BatchIndex.Uint64())
+		recordBatchSubmission := bindings.IRecordBatchSubmission{
+			Index:      rollupCommitBatch.BatchIndex,
+			Submitter:  msg.From(),
+			StartBlock: big.NewInt(int64(batchData.FirstBlockNumber())),
+			EndBlock:   big.NewInt(int64(batchData.LastBlockNumber())),
+			RollupTime: big.NewInt(int64(header.Time)),
+		}
+		recordBatchSubmissiones = append(recordBatchSubmissiones, recordBatchSubmission)
+	}
+	return recordBatchSubmissiones, nil
+}
+
+func (o *Oracle) fetchRollupLog(ctx context.Context, start, end uint64) ([]types.Log, error) {
+	query := ethereum.FilterQuery{
+		FromBlock: big.NewInt(0).SetUint64(start),
+		ToBlock:   big.NewInt(0).SetUint64(end),
+		Addresses: []common.Address{
+			o.rollupAddr,
+		},
+		Topics: [][]common.Hash{
+			{RollupEventTopicHash},
+		},
+	}
+	return o.l1Client.FilterLogs(ctx, query)
+}
+
+func (o *Oracle) GetNextBatchSubmissionIndex() (*big.Int, error) {
+	// TODO
+	return o.record.NextBatchSubmissionIndex(nil)
+}
+
+func (o *Oracle) submitRecord() {
+	nextBatchSubmissionIndex, err := o.GetNextBatchSubmissionIndex()
+	lastFinalized, err := o.rollup.LastFinalizedBatchIndex(nil)
+	if err != nil {
+		// TODO
+	}
+	if nextBatchSubmissionIndex.Cmp(lastFinalized) > 0 {
+		// TODO
+		return
+	}
+	start := nextBatchSubmissionIndex.Uint64()
+	end := lastFinalized.Uint64()
+	if start+o.cfg.MaxSize < end {
+		end = start + o.cfg.MaxSize - 1
+	}
+	batchSubmissiones, err := o.GetBatchSubmission(context.Background(), start, end)
+	tx, err := o.record.RecordFinalizedBatchSubmissions(nil, batchSubmissiones)
+	receipt, err := o.l2Client.TransactionReceipt(context.Background(), tx.Hash())
+	if err != nil {
+
+	}
+	if receipt.Status != types.ReceiptStatusSuccessful {
+
+	}
+}
--- oracle/staking-oracle/main.go
+++ oracle/staking-oracle/main.go
@@ -2,23 +2,60 @@ package main
 
 import (
 	"context"
-	"github.com/morph-l2/bindings/bindings"
-	"github.com/scroll-tech/go-ethereum"
+	"fmt"
+	"github.com/ethereum/go-ethereum"
+	"io"
+	"math/big"
+	"os"
 	"time"
 
+	"github.com/morph-l2/bindings/bindings"
+	"github.com/morph-l2/morph/oracle/backoff"
+	"github.com/morph-l2/node/derivation"
 	"github.com/scroll-tech/go-ethereum/accounts/abi/bind"
+	"github.com/scroll-tech/go-ethereum/common"
+	"github.com/scroll-tech/go-ethereum/core/types"
 	"github.com/scroll-tech/go-ethereum/ethclient"
+	"github.com/scroll-tech/go-ethereum/log"
+	tmhttp "github.com/tendermint/tendermint/rpc/client/http"
+	tmtypes "github.com/tendermint/tendermint/types"
+	"gopkg.in/natefinch/lumberjack.v2"
 )
+
+var defaultRewardEpoch = time.Hour * 24
+
+type Config struct {
+	RollupAddr    common.Address
+	L2StakingAddr common.Address
+	L1Rpc         string
+	L2Rpc         string
+	TendermintRpc string
+	WsEndpoint    string
+	MaxSize       uint64
+
+	LogFilename    string
+	LogFileMaxSize int
+	LogFileMaxAge  int
+	LogCompress    bool
+	LogTerminal    bool
+	LogLevel       string
+}
 
 type Oracle struct {
 	ctx                 context.Context
 	l1Client            DeployContractBackend
 	l2Client            *ethclient.Client
-	staking             bindings.Staking
+	l2Staking           *bindings.L2Staking
+	rollup              *bindings.Rollup
+	rollupAddr          common.Address
+	record              *bindings.Record
+	TmClient            *tmhttp.HTTP
 	cancel              context.CancelFunc
 	pollInterval        time.Duration
+	rewardEpoch         time.Duration
 	logProgressInterval time.Duration
 	stop                chan struct{}
+	cfg                 *Config
 }
 
 type DeployContractBackend interface {
@@ -28,6 +65,238 @@ type DeployContractBackend interface {
 	ethereum.TransactionReader
 }
 
-func NewOracle() {
+func NewOracle(cfg *Config) (*Oracle, error) {
+	var logHandler log.Handler
 
+	output := io.Writer(os.Stderr)
+	if cfg.LogFilename != "" {
+		f, err := os.OpenFile(cfg.LogFilename, os.O_CREATE|os.O_RDWR, os.FileMode(0600))
+		if err != nil {
+			return nil, fmt.Errorf("wrong log.filename set: %d", err)
+		}
+		f.Close()
+
+		if cfg.LogFileMaxSize < 1 {
+			return nil, fmt.Errorf("wrong log.maxsize set: %d", cfg.LogFileMaxSize)
+		}
+
+		if cfg.LogFileMaxAge < 1 {
+			return nil, fmt.Errorf("wrong log.maxage set: %d", cfg.LogFileMaxAge)
+		}
+		logFile := &lumberjack.Logger{
+			Filename: cfg.LogFilename,
+			MaxSize:  cfg.LogFileMaxSize, // megabytes
+			MaxAge:   cfg.LogFileMaxAge,  // days
+			Compress: cfg.LogCompress,
+		}
+		output = io.MultiWriter(output, logFile)
+	}
+
+	if cfg.LogTerminal {
+		logHandler = log.StreamHandler(os.Stdout, log.TerminalFormat(true))
+	} else {
+		logHandler = log.StreamHandler(output, log.JSONFormat())
+	}
+
+	logLevel, err := log.LvlFromString(cfg.LogLevel)
+	if err != nil {
+		return nil, err
+	}
+
+	log.Root().SetHandler(log.LvlFilterHandler(logLevel, logHandler))
+	l1Client, err := ethclient.Dial(cfg.L1Rpc)
+	if err != nil {
+		panic(err)
+	}
+	l2Client, err := ethclient.Dial(cfg.L2Rpc)
+	if err != nil {
+		panic(err)
+	}
+	tmClient, err := tmhttp.New(cfg.TendermintRpc, cfg.WsEndpoint)
+	if err != nil {
+		panic(err)
+	}
+
+	rollup, err := bindings.NewRollup(cfg.RollupAddr, l1Client)
+	if err != nil {
+		panic(err)
+	}
+	l2Staking, err := bindings.NewL2Staking(cfg.L2StakingAddr, l2Client)
+
+	return &Oracle{
+		l1Client:  l1Client,
+		l2Client:  l2Client,
+		rollup:    rollup,
+		l2Staking: l2Staking,
+		TmClient:  tmClient,
+		cfg:       cfg,
+	}, nil
+}
+
+func (o *Oracle) loop() {
+	// block node startup during initial sync and print some helpful logs
+
+	go func() {
+		t := time.NewTicker(o.pollInterval)
+		defer t.Stop()
+
+		for {
+			// don't wait for ticker during startup
+			o.syncRewardEpochs()
+			select {
+			case <-o.ctx.Done():
+				log.Error("derivation node Unexpected exit")
+				close(o.stop)
+				return
+			case <-t.C:
+				continue
+			}
+		}
+	}()
+}
+
+func (o *Oracle) syncRewardEpochs() error {
+	finalizedBlockTime, err := o.getFinalizedBlockTime()
+	if err != nil {
+		return err
+	}
+	syncedBlockTime := o.getSyncedBlockTime()
+	if finalizedBlockTime > syncedBlockTime {
+		epochNum := int64(finalizedBlockTime) - int64(syncedBlockTime)/o.rewardEpoch.Nanoseconds()
+		for i := 0; i < int(epochNum); i++ {
+			o.syncRewardEpoch()
+		}
+	}
+	return nil
+}
+
+func (o *Oracle) getSyncedBlockTime() uint64 {
+	// TODO
+	return 0
+}
+
+func (o *Oracle) getFinalizedBlockTime() (uint64, error) {
+	latestFinalized, err := o.rollup.LastFinalizedBatchIndex(nil)
+	if err != nil {
+		return 0, err
+	}
+	batch, err := o.l2Client.GetRollupBatchByIndex(context.Background(), latestFinalized.Uint64())
+	if err != nil {
+		return 0, err
+	}
+	if batch == nil {
+		// TODO
+		return 0, fmt.Errorf("batch not found")
+	}
+	var batchData derivation.BatchInfo
+	if err = batchData.ParseBatch(*batch); err != nil {
+		return 0, fmt.Errorf("parse batch error:%v", err)
+	}
+	lastBlockNumber := batchData.LastBlockNumber()
+	header, err := o.l2Client.HeaderByNumber(o.ctx, big.NewInt(int64(lastBlockNumber)))
+	if err != nil {
+		return 0, err
+	}
+	return header.Time, nil
+}
+
+func (o *Oracle) syncRewardEpoch() {
+
+	recordRewardEpochInfo, err := o.getRewardEpochs()
+	if err != nil {
+		return
+	}
+	tx, err := o.record.RecordRewardEpochs(nil, []bindings.IRecordRewardEpochInfo{*recordRewardEpochInfo})
+	receipt, err := o.l2Client.TransactionReceipt(context.Background(), tx.Hash())
+	if err != nil {
+
+	}
+	if receipt.Status != types.ReceiptStatusSuccessful {
+
+	}
+}
+
+func (o *Oracle) getRewardEpochs() (*bindings.IRecordRewardEpochInfo, error) {
+	// reward
+	// TODO
+	nextRewardEpochIndex, err := o.record.NextRewardEpochIndex(nil)
+	if err != nil {
+		return nil, err
+	}
+	startHeight := o.getNextHeight()
+	nextTime, err := o.getNextTime(nextRewardEpochIndex)
+	if err != nil {
+		return nil, err
+	}
+	height := startHeight
+	sequencersBlockCount := make(map[common.Address]int64)
+	for {
+		tmHeader, err := o.L2HeaderByNumberWithRetry(height)
+		if err != nil {
+			return nil, err
+		}
+		if tmHeader.Time.Unix() > nextTime.Int64() {
+			break
+		}
+		sequencer := o.getSequencer(tmHeader.ProposerAddress)
+		sequencersBlockCount[sequencer] += 1
+	}
+	var sequencers []common.Address
+	var blockCounts []*big.Int
+	for seq, count := range sequencersBlockCount {
+		sequencers = append(sequencers, seq)
+		blockCounts = append(blockCounts, big.NewInt(count))
+	}
+	rewardEpochInfo := bindings.IRecordRewardEpochInfo{
+		Index:           nextRewardEpochIndex,
+		BlockCount:      big.NewInt(height - startHeight + 1),
+		Sequencers:      sequencers,
+		SequencerBlocks: blockCounts,
+		// TODO
+		SequencerRatios:      nil,
+		SequencerCommissions: nil,
+	}
+	return &rewardEpochInfo, nil
+}
+
+func (o *Oracle) getHeader(height int64) (*tmtypes.Header, error) {
+	headerResp, err := o.TmClient.Header(context.Background(), &height)
+	if err != nil {
+		return nil, err
+	}
+	return headerResp.Header, nil
+}
+
+// L2HeaderByNumberWithRetry retries getting headers.
+func (o *Oracle) L2HeaderByNumberWithRetry(height int64) (*tmtypes.Header, error) {
+	var res *tmtypes.Header
+	err := backoff.DoCtx(o.ctx, 3, backoff.Exponential(), func() error {
+		var err error
+		headerResp, err := o.TmClient.Header(context.Background(), &height)
+		if err != nil {
+			return err
+		}
+		res = headerResp.Header
+		return nil
+	})
+	return res, err
+}
+
+func (o *Oracle) getSequencer(proposerAddress tmtypes.Address) common.Address {
+	// TODO
+	return common.Address{}
+}
+
+func (o *Oracle) getNextHeight() int64 {
+	// TODO
+	return 0
+}
+
+func (o *Oracle) getNextTime(nextRewardEpochIndex *big.Int) (*big.Int, error) {
+	// TODO check start index 0 or 1
+	startTime, err := o.l2Staking.REWARDSTARTTIME(nil)
+	if err != nil {
+		return nil, err
+	}
+	return startTime.Add(startTime, nextRewardEpochIndex.Add(nextRewardEpochIndex, big.NewInt(int64(o.rewardEpoch)))), nil
 }
