428fb28333d24dce5e8c6cd7fff502fbfc103360
marvel.yu <marvel.yu@bitget.com>
2024-05-06 12:39:10 +0800

    add sequencer size check when init staking

--- contracts/deploy/018-StakingInit.ts
+++ contracts/deploy/018-StakingInit.ts
@@ -87,16 +87,18 @@ export const StakingInit = async (
         console.log('Upgrading the Staking proxy...')
         const admin: string = configTmp.contractAdmin
         const sequencerSize: number = configTmp.stakingSequencerSize
+        const sequencerWhiteList = configTmp.l2SequencerAddresses
         const limit: number = configTmp.stakingMinDeposit
         const lock: number = configTmp.stakingLockNumber
 
         if (!ethers.utils.isAddress(admin)
             || !ethers.utils.isAddress(L1SequencerProxyAddress)
             || sequencerSize == 0
+            || sequencerSize != sequencerWhiteList.length
             || lock == 0
             || limit == 0
         ) {
-            console.error('please check your address')
+            console.error('please check your address and your sequencer array size')
             return ''
         }
 
