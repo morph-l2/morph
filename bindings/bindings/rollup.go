cf58ef3763cae1e7764586872908bad35bd702c5
Segue <huoda.china@163.com>
2024-05-15 23:18:20 +0800

    update bindings

--- bindings/bindings/rollup.go
+++ bindings/bindings/rollup.go
@@ -26,7 +26,6 @@ var (
 	_ = common.Big1
 	_ = types.BloomLookup
 	_ = event.NewSubscription
-	_ = abi.ConvertType
 )
 
 // IRollupBatchDataInput is an auto generated low-level Go binding around an user-defined struct.
@@ -175,11 +174,11 @@ func NewRollupFilterer(address common.Address, filterer bind.ContractFilterer) (
 
 // bindRollup binds a generic wrapper to an already deployed contract.
 func bindRollup(address common.Address, caller bind.ContractCaller, transactor bind.ContractTransactor, filterer bind.ContractFilterer) (*bind.BoundContract, error) {
-	parsed, err := RollupMetaData.GetAbi()
+	parsed, err := abi.JSON(strings.NewReader(RollupABI))
 	if err != nil {
 		return nil, err
 	}
-	return bind.NewBoundContract(address, *parsed, caller, transactor, filterer), nil
+	return bind.NewBoundContract(address, parsed, caller, transactor, filterer), nil
 }
 
 // Call invokes the (constant) contract method with params as input values and
