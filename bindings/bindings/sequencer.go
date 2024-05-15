d6c96bdb7a10b7209cf519ab4769eb1b1029c41b
ryanmorphl2 <163962984+ryanmorphl2@users.noreply.github.com>
2024-05-15 15:42:53 +0800

    update bindings

--- bindings/bindings/sequencer.go
+++ bindings/bindings/sequencer.go
@@ -26,6 +26,7 @@ var (
 	_ = common.Big1
 	_ = types.BloomLookup
 	_ = event.NewSubscription
+	_ = abi.ConvertType
 )
 
 // SequencerMetaData contains all meta data concerning the Sequencer contract.
@@ -156,11 +157,11 @@ func NewSequencerFilterer(address common.Address, filterer bind.ContractFilterer
 
 // bindSequencer binds a generic wrapper to an already deployed contract.
 func bindSequencer(address common.Address, caller bind.ContractCaller, transactor bind.ContractTransactor, filterer bind.ContractFilterer) (*bind.BoundContract, error) {
-	parsed, err := abi.JSON(strings.NewReader(SequencerABI))
+	parsed, err := SequencerMetaData.GetAbi()
 	if err != nil {
 		return nil, err
 	}
-	return bind.NewBoundContract(address, parsed, caller, transactor, filterer), nil
+	return bind.NewBoundContract(address, *parsed, caller, transactor, filterer), nil
 }
 
 // Call invokes the (constant) contract method with params as input values and
