10694e1c9357d65266241998206c1e6de6597bd9
Ryan He <163962984+ryanmorphl2@users.noreply.github.com>
2024-06-25 14:43:48 +0800

    update gas oracle contract (#389)

--- contracts/contracts/l2/system/GasPriceOracle.sol
+++ contracts/contracts/l2/system/GasPriceOracle.sol
@@ -22,6 +22,22 @@ contract GasPriceOracle is Ownable {
     ///      x1000 should be enough.
     uint256 private constant MAX_SCALE = 1000 * PRECISION;
 
+    /// @dev The maximum possible l1 commit fee scalar.
+    /// We derive the commit scalar by
+    /// ```
+    /// commit_scalar = commit_gas_per_tx * fluctuation_multiplier * 1e9
+    /// ```
+    /// So, the value should not exceed 10^9 * 1e9 normally.
+    uint256 private constant MAX_COMMIT_SCALAR = 10 ** 9 * PRECISION;
+
+    /// @dev The maximum possible l1 blob fee scalar.
+    /// We derive the blob scalar by
+    /// ```
+    /// blob_scalar = fluctuation_multiplier / compression_ratio / blob_util_ratio * 1e9
+    /// ```
+    /// So, the value should not exceed 10^9 * 1e9 normally.
+    uint256 private constant MAX_BLOB_SCALAR = 10 ** 9 * PRECISION;
+
     /*//////////////////////////////////////////////////////////////
                                STORAGE
     //////////////////////////////////////////////////////////////*/
@@ -35,6 +51,14 @@ contract GasPriceOracle is Ownable {
     bool public allowListEnabled;
     // Address list with permission to update price oracle.
     mapping(address => bool) public isAllowed;
+    /// l1 blob base fee
+    uint256 public l1BlobBaseFee;
+    /// commit scalar
+    uint256 public commitScalar;
+    /// blob scalar
+    uint256 public blobScalar;
+    /// @notice Indicates whether the network has gone through the Curie upgrade.
+    bool public isCurie;
 
     /*//////////////////////////////////////////////////////////////
                                 EVENTS
@@ -44,6 +68,36 @@ contract GasPriceOracle is Ownable {
     event L1BaseFeeUpdated(uint256);
     event OverheadUpdated(uint256);
     event ScalarUpdated(uint256);
+    event L1BlobBaseFeeUpdated(uint256 l1BlobBaseFee);
+    event CommitScalarUpdated(uint256 scalar);
+    event BlobScalarUpdated(uint256 scalar);
+
+    /*//////////////////////////////////////////////////////////////
+                               Errors 
+    //////////////////////////////////////////////////////////////*/
+    /// @dev Thrown when the blob fee scalar exceed MAX_BLOB_SCALAR.
+    error ErrExceedMaxBlobScalar();
+
+    /// @dev Thrown when the commit fee scalar exceed MAX_COMMIT_SCALAR.
+    error ErrExceedMaxCommitScalar();
+
+    /// @dev Thrown when the l1 fee overhead exceed MAX_OVERHEAD.
+    error ErrExceedMaxOverhead();
+
+    /// @dev Thrown when the l1 fee scalar exceed MAX_SCALAR.
+    error ErrExceedMaxScalar();
+
+    /// @dev Thrown when the caller is not in allowed list.
+    error ErrCallerNotAllowed();
+
+    /// @dev Thrown when setting the same value.
+    error ErrSettintSameValue();
+
+    /// @dev Thrown when setting values to different lengths for keys.
+    error ErrDifferentLength();
+
+    /// @dev Thrown when we enable Curie fork after Curie fork.
+    error ErrAlreadyInCurieFork();
 
     /**
      * @param owner_ Address that will initially own this contract.
@@ -57,7 +111,7 @@ contract GasPriceOracle is Ownable {
                              ALLOWLIST
     //////////////////////////////////////////////////////////////*/
     function setAllowList(address[] memory user, bool[] memory val) external onlyOwner {
-        require(user.length == val.length, "INVALID_INPUT");
+        if (user.length != val.length) revert ErrDifferentLength();
 
         for (uint256 i = 0; i < user.length; i++) {
             isAllowed[user[i]] = val[i];
@@ -66,26 +120,113 @@ contract GasPriceOracle is Ownable {
     }
 
     function setAllowListEnabled(bool _allowListEnabled) external onlyOwner {
-        require(_allowListEnabled != allowListEnabled, "ALREADY_SET");
+        if (_allowListEnabled == allowListEnabled) revert ErrSettintSameValue();
+
         allowListEnabled = _allowListEnabled;
         emit AllowListEnabledUpdated(_allowListEnabled);
     }
 
     modifier onlyAllowed() {
         // solhint-disable-next-line avoid-tx-origin
-        require(owner() == msg.sender || (allowListEnabled && isAllowed[msg.sender]), "not allowed");
+        if (!(owner() == msg.sender || (allowListEnabled && isAllowed[msg.sender]))) {
+            revert ErrCallerNotAllowed();
+        }
         _;
     }
 
+    /// Compatibility with old logic
     function getL1Fee(bytes memory _data) external view returns (uint256) {
-        uint256 _l1GasUsed = getL1GasUsed(_data);
-        uint256 _l1Fee = _l1GasUsed * l1BaseFee;
-        return (_l1Fee * scalar) / PRECISION;
+        if (isCurie) {
+            return getL1FeeCurie(_data);
+        } else {
+            return getL1FeeBeforeCurie(_data);
+        }
     }
 
-    /// @dev The `_data` is the RLP-encoded transaction with signature. And we also reserve additional
-    ///      4 bytes in the non-zero bytes to store the number of bytes in the RLP-encoded transaction.
-    function getL1GasUsed(bytes memory _data) public view returns (uint256) {
+    /// Compatibility with old logic
+    function getL1GasUsed(bytes memory _data) external view returns (uint256) {
+        if (isCurie) {
+            // It is near zero since we put all transactions to blob.
+            return 0;
+        } else {
+            return getL1GasUsedBeforeCurie(_data);
+        }
+    }
+
+    /// Allows the owner to modify the l1 base fee.
+    /// @param _l1BaseFee New l1 base fee
+    // slither-disable-next-line external-function
+    function setL1BaseFee(uint256 _l1BaseFee) external onlyAllowed {
+        l1BaseFee = _l1BaseFee;
+        emit L1BaseFeeUpdated(_l1BaseFee);
+    }
+
+    /// @notice Allows the owner to modify the l1 base fee.
+    /// @param _l1BaseFee New l1 base fee
+    /// @param _l1BlobBaseFee New l1 blob base fee
+    // slither-disable-next-line external-function
+    function setL1BaseFeeAndBlobBaseFee(uint256 _l1BaseFee, uint256 _l1BlobBaseFee) external onlyAllowed {
+        l1BaseFee = _l1BaseFee;
+        l1BlobBaseFee = _l1BlobBaseFee;
+
+        emit L1BaseFeeUpdated(_l1BaseFee);
+        emit L1BlobBaseFeeUpdated(_l1BlobBaseFee);
+    }
+
+    /// @notice Allows the owner to modify the overhead.
+    /// @param _overhead New overhead
+    // slither-disable-next-line external-function
+    function setOverhead(uint256 _overhead) external onlyAllowed {
+        if (_overhead > MAX_OVERHEAD) revert ErrExceedMaxOverhead();
+
+        overhead = _overhead;
+        emit OverheadUpdated(_overhead);
+    }
+
+    /// @notice Allows the owner to modify the scalar.
+    /// @param _scalar New scalar
+    // slither-disable-next-line external-function
+    function setScalar(uint256 _scalar) external onlyAllowed {
+        if (_scalar > MAX_SCALE) revert ErrExceedMaxScalar();
+
+        scalar = _scalar;
+        emit ScalarUpdated(_scalar);
+    }
+
+    /// @notice Allows the owner to modify the commit scalar.
+    /// @param _scalar New scalar
+    function setCommitScalar(uint256 _scalar) external onlyAllowed {
+        if (_scalar > MAX_COMMIT_SCALAR) revert ErrExceedMaxCommitScalar();
+
+        commitScalar = _scalar;
+        emit CommitScalarUpdated(_scalar);
+    }
+
+    /// Allows the owner to modify the blob scalar.
+    /// @param _scalar New scalar
+    function setBlobScalar(uint256 _scalar) external onlyAllowed {
+        if (_scalar > MAX_BLOB_SCALAR) revert ErrExceedMaxBlobScalar();
+
+        blobScalar = _scalar;
+        emit BlobScalarUpdated(_scalar);
+    }
+
+    /// @notice Enable the Curie fork.
+    function enableCurie() external onlyAllowed {
+        if (isCurie) revert ErrAlreadyInCurieFork();
+        isCurie = true;
+    }
+
+    /*//////////////////////////////////////////////////////////////
+                        * Internal Functions * 
+    //////////////////////////////////////////////////////////////*/
+
+    /// @dev Internal function to computes the amount of L1 gas used for a transaction before Curie fork.
+    ///   The `_data` is the RLP-encoded transaction with signature. And we also reserve additional
+    ///   4 bytes in the non-zero bytes to store the number of bytes in the RLP-encoded transaction.
+    /// @param _data Signed fully RLP-encoded transaction to get the L1 gas for.
+    /// @return Amount of L1 gas used to publish the transaction.
+    function getL1GasUsedBeforeCurie(bytes memory _data) internal view returns (uint256) {
         uint256 _total = 0;
         uint256 _length = _data.length;
         unchecked {
@@ -100,37 +241,22 @@ contract GasPriceOracle is Ownable {
         }
     }
 
-    /**
-     * Allows the owner to modify the l1 base fee.
-     * @param _baseFee New l1 base fee
-     */
-    // slither-disable-next-line external-function
-    function setL1BaseFee(uint256 _baseFee) external onlyAllowed {
-        l1BaseFee = _baseFee;
-        emit L1BaseFeeUpdated(_baseFee);
+    /// @dev Internal function to compute the L1 portion of the fee based on the size of the rlp encoded input
+    ///   transaction, the current L1 base fee, and the various dynamic parameters, before Curie fork.
+    /// @param _data Signed fully RLP-encoded transaction to get the L1 fee for.
+    /// @return L1 fee that should be paid for the tx
+    function getL1FeeBeforeCurie(bytes memory _data) internal view returns (uint256) {
+        uint256 _l1GasUsed = getL1GasUsedBeforeCurie(_data);
+        uint256 _l1Fee = _l1GasUsed * l1BaseFee;
+        return (_l1Fee * scalar) / PRECISION;
     }
 
-    /**
-     * Allows the owner to modify the overhead.
-     * @param _overhead New overhead
-     */
-    // slither-disable-next-line external-function
-    function setOverhead(uint256 _overhead) external onlyAllowed {
-        require(_overhead <= MAX_OVERHEAD, "exceed maximum overhead");
-
-        overhead = _overhead;
-        emit OverheadUpdated(_overhead);
-    }
-
-    /**
-     * Allows the owner to modify the scalar.
-     * @param _scalar New scalar
-     */
-    // slither-disable-next-line external-function
-    function setScalar(uint256 _scalar) external onlyAllowed {
-        require(_scalar <= MAX_SCALE, "exceed maximum scale");
-
-        scalar = _scalar;
-        emit ScalarUpdated(_scalar);
+    /// @dev Internal function to compute the L1 portion of the fee based on the size of the rlp encoded input
+    ///   transaction, the current L1 base fee, and the various dynamic parameters, after Curie fork.
+    /// @param _data Signed fully RLP-encoded transaction to get the L1 fee for.
+    /// @return L1 fee that should be paid for the tx
+    function getL1FeeCurie(bytes memory _data) internal view returns (uint256) {
+        // We have bounded the value of `commitScalar` and `blobScalar`, the whole expression won't overflow.
+        return (commitScalar * l1BaseFee + blobScalar * _data.length * l1BlobBaseFee) / PRECISION;
     }
 }
--- contracts/tasks/proxy_upgrade.ts
+++ contracts/tasks/proxy_upgrade.ts
@@ -253,3 +253,72 @@ task("upgradeRollupProxy")
         const rec = await res.wait()
         console.log(`upgrade rollup ${rec.status === 1}`)
     })
+
+/*
+Proxy__GasOracle
+
+EnableCurie
+
+yarn hardhat gasOracleEnableCurie --gasoracleproxyaddr 0x530000000000000000000000000000000000000f --network l2
+*/
+task("gasOracleEnableCurie")
+    .addParam("gasoracleproxyaddr")
+    .setAction(async (taskArgs, hre) => {
+        if (!hre.ethers.utils.isAddress(taskArgs.gasoracleproxyaddr)) {
+            console.log(
+                `GasOracle proxy address check failed ${taskArgs.gasoracleproxyaddr}`
+            )
+            return
+        }
+
+        let iGasOracle = await hre.ethers.getContractAt("GasPriceOracle", taskArgs.gasoracleproxyaddr)
+        let isCurie = await iGasOracle.isCurie()
+
+        if (isCurie) {
+            console.log("Already set isCurie")
+            return
+        }
+
+        // enable curie
+        const res = await iGasOracle.enableCurie()
+        const rec = await res.wait()
+        isCurie = await iGasOracle.isCurie()
+        console.log(`Enable curie ${isCurie === true ? "succeed" : "failed"}`)
+    })
+
+/*
+Proxy__GasOracle
+
+yarn hardhat upgradeGasOracleProxy --proxyadminaddr 0x530000000000000000000000000000000000000B --gasoracleproxyaddr 0x530000000000000000000000000000000000000f --network l2
+*/
+task("upgradeGasOracleProxy")
+    .addParam("proxyadminaddr")
+    .addParam("gasoracleproxyaddr")
+    .setAction(async (taskArgs, hre) => {
+        if (
+            !hre.ethers.utils.isAddress(taskArgs.gasoracleproxyaddr) ||
+            !hre.ethers.utils.isAddress(taskArgs.proxyadminaddr)
+        ) {
+            console.log(
+                `GasOracle proxy address check failed ${taskArgs.gasoracleproxyaddr} ${taskArgs.proxyadminaddr}`
+            )
+            return
+        }
+
+        const proxyAdminFactory = await hre.ethers.getContractFactory("ProxyAdmin")
+        const proxyAdmin = proxyAdminFactory.attach(taskArgs.proxyadminaddr)
+        const owner = await proxyAdmin.owner()
+
+        // upgrade
+        const gasOracleFactory = await hre.ethers.getContractFactory("GasPriceOracle")
+        const gasOracleNewImpl = await gasOracleFactory.deploy(owner)
+        await gasOracleNewImpl.deployed()
+        console.log("New implementation address of gasOracle: ", gasOracleNewImpl.address)
+        if (!hre.ethers.utils.isAddress(gasOracleNewImpl.address)) {
+            console.log(`New implementation address of gasOracle cheked failed ${gasOracleNewImpl.address}`)
+            return
+        }
+        const res = await proxyAdmin.upgrade(taskArgs.gasoracleproxyaddr, gasOracleNewImpl.address)
+        const rec = await res.wait()
+        console.log(`Upgrade gasOracle ${rec.status === 1 ? "succeed" : "failed"}`)
+    })
