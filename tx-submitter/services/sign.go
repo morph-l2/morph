80af5ef027c6ad68b27cc52b798630e0fe154b94
WorldDogs <33647825+WorldDogs@users.noreply.github.com>
2024-07-19 16:23:03 +0800

    enhance the sign method

--- tx-submitter/services/sign.go
+++ tx-submitter/services/sign.go
@@ -3,13 +3,24 @@ package services
 import (
 	"fmt"
 
+	"morphl2/externalsign"
+
 	"github.com/scroll-tech/go-ethereum/core/types"
 )
 
+var externalSigner *externalsign.ExternalSign
+
 func (r *Rollup) Sign(tx *types.Transaction) (*types.Transaction, error) {
 	if r.cfg.ExternalSign {
-		//todo
-		return nil, fmt.Errorf("not support external sign yet")
+		if externalSigner == nil {
+			externalsign.NewExternalSign(r.cfg.ExternalSignAppid, r.privKey, r.cfg.ExternalSignUrl)
+		}
+
+		signedTx, err := externalsigner.RequestSign([]types.Transaction{*tx})
+		if err != nil {
+			return nil, fmt.Errorf("externalsign sign tx error:%v", err)
+		}
+		return signedTx, nil
 	} else {
 		signedTx, err := types.SignTx(tx, types.NewLondonSignerWithEIP4844(r.chainId), r.privKey)
 		if err != nil {
