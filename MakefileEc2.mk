d64be6f95d0c9801f5ffe88a51cac61f4dd88939
marvel.yu <marvel.yu@bitget.com>
2024-08-16 15:41:07 +0800

    add upload to s3

--- MakefileEc2.mk
+++ MakefileEc2.mk
@@ -3,3 +3,5 @@ build-bk-prod-morph-prod-mainnet-to-morph-node:
 	cd $(PWD)/node && make build
 	cp node/build/bin/morphnode dist/
 	cp node/build/bin/tendermint dist/
+	tar -czvf morph-node.tar.gz dist
+	aws s3 cp morph-node.tar.gz s3://morph-0582-morph-technical-department-mainnet-data/morph-setup/morph-node.tar.gz
