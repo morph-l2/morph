2f229f5e616bb57a8153e5ea6ceb7b72c447e044
Segue <huoda.china@163.com>
2024-08-26 15:51:30 +0800

    update bindings

--- bindings/bindings/distribute.go
+++ bindings/bindings/distribute.go
@@ -26,6 +26,7 @@ var (
 	_ = common.Big1
 	_ = types.BloomLookup
 	_ = event.NewSubscription
+	_ = abi.ConvertType
 )
 
 // DistributeMetaData contains all meta data concerning the Distribute contract.
@@ -156,11 +157,11 @@ func NewDistributeFilterer(address common.Address, filterer bind.ContractFiltere
 
 // bindDistribute binds a generic wrapper to an already deployed contract.
 func bindDistribute(address common.Address, caller bind.ContractCaller, transactor bind.ContractTransactor, filterer bind.ContractFilterer) (*bind.BoundContract, error) {
-	parsed, err := abi.JSON(strings.NewReader(DistributeABI))
+	parsed, err := DistributeMetaData.GetAbi()
 	if err != nil {
 		return nil, err
 	}
-	return bind.NewBoundContract(address, parsed, caller, transactor, filterer), nil
+	return bind.NewBoundContract(address, *parsed, caller, transactor, filterer), nil
 }
 
 // Call invokes the (constant) contract method with params as input values and
