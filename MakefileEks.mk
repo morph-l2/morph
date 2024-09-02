c7b7c3ac782a267dc985f2cad2eb0f689159796d
marvel.yu <marvel.yu@bitget.com>
2024-09-02 16:07:27 +0800

    update makefile for eks build

--- MakefileEks.mk
+++ MakefileEks.mk
@@ -5,6 +5,8 @@ build-bk-prod-morph-prod-mainnet-to-morph-gas-price-oracle:
 	if [ ! -d dist ]; then mkdir -p dist; fi
 	cd $(PWD)/gas-oracle/app && cargo build --release
 	cp gas-oracle/app/target/release/app dist/
+	aws s3 cp s3://morph-0582-morph-technical-department-mainnet-data/morph-setup/secret-manager-wrapper.tar.gz ./
+	tar -xvzf secret-manager-wrapper.tar.gz
 
 start-bk-prod-morph-prod-mainnet-to-morph-gas-price-oracle: export GAS_ORACLE_EXTERNAL_SIGN=true
 start-bk-prod-morph-prod-mainnet-to-morph-gas-price-oracle: export GAS_ORACLE_EXTERNAL_SIGN_ADDRESS=0x0000000000000000000000000000000000000000
@@ -29,7 +31,7 @@ start-bk-prod-morph-prod-mainnet-to-morph-gas-price-oracle: export L1_ROLLUP=0x1
 start-bk-prod-morph-prod-mainnet-to-morph-gas-price-oracle: export GAS_ORACLE_L1_BASE_FEE_BUFFER=100000000
 start-bk-prod-morph-prod-mainnet-to-morph-gas-price-oracle: export GAS_ORACLE_COMMIT_SCALAR_BUFFER=20000000000
 start-bk-prod-morph-prod-mainnet-to-morph-gas-price-oracle:
-	./app
+	/data/secret-manager-wrapper ./app
 
 
 # prover
@@ -57,6 +59,8 @@ build-bk-prod-morph-prod-mainnet-to-morph-challenge-handler:
 	if [ ! -d dist ]; then mkdir -p dist; fi
 	cd $(PWD)/prover/challenge-handler && cargo build --release
 	cp prover/challenge-handler/target/release/challenge-handler dist/
+	aws s3 cp s3://morph-0582-morph-technical-department-mainnet-data/morph-setup/secret-manager-wrapper.tar.gz ./
+	tar -xvzf secret-manager-wrapper.tar.gz
 
 start-bk-prod-morph-prod-mainnet-to-morph-challenge-handler: export HANDLER_EXTERNAL_SIGN=true
 start-bk-prod-morph-prod-mainnet-to-morph-challenge-handler: export HANDLER_EXTERNAL_SIGN_ADDRESS=0x0000000000000000000000000000000000000000
@@ -70,7 +74,7 @@ start-bk-prod-morph-prod-mainnet-to-morph-challenge-handler: export HANDLER_PROV
 start-bk-prod-morph-prod-mainnet-to-morph-challenge-handler: export HANDLER_L1_ROLLUP=0x1dc010026af6fd4a6dc3686446c752094bda8d4d
 start-bk-prod-morph-prod-mainnet-to-morph-challenge-handler: export CHALLENGE_HANDLER_PRIVATE_KEY=0x1
 start-bk-prod-morph-prod-mainnet-to-morph-challenge-handler:
-	./challenge-handler
+	/data/secret-manager-wrapper ./challenge-handler
 
 
 # shadow-proving
