85e9c6b20e9d60f83d738d36862ffba04d428fa2
anylots <username@users.noreply.github.com>
2024-09-10 23:01:44 +0800

    snark prove

--- prover/tests/keccak256/host/src/main.rs
+++ prover/tests/keccak256/host/src/main.rs
@@ -21,10 +21,16 @@ fn main() {
     let (public_values, execution_report) = client.execute(dev_elf, stdin.clone()).run().unwrap();
     println!("Program executed successfully.");
     // Record the number of cycles executed.
-    println!("Number of cycles: {}", execution_report.total_instruction_count());
+    println!(
+        "Number of cycles: {}",
+        execution_report.total_instruction_count()
+    );
 
     let rt_data = public_values.as_slice();
-    println!("pi_hash generated with sp1-vm execution: {}", hex::encode(rt_data));
+    println!(
+        "pi_hash generated with sp1-vm execution: {}",
+        hex::encode(rt_data)
+    );
 
     let start = Instant::now();
 
@@ -32,10 +38,17 @@ fn main() {
     let (pk, vk) = client.setup(dev_elf);
 
     // Generate the proof
-    let proof = client.prove(&pk, stdin).run().expect("failed to generate proof");
+    let proof = client
+        .prove(&pk, stdin)
+        .plonk()
+        .run()
+        .expect("failed to generate proof");
 
-    let duration_secs = start.elapsed().as_secs();
-    println!("Successfully generated proof!, time use: {:?} secs", duration_secs);
+    let duration_mins = start.elapsed().as_secs() / 60;
+    println!(
+        "Successfully generated proof!, time use: {:?} minutes",
+        duration_mins
+    );
 
     // Verify the proof.
     client.verify(&proof, &vk).expect("failed to verify proof");
