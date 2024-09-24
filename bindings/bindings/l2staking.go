cd771b702b0c87ece66b31eb923dcfc024606df0
kukoomomo <kukoomomo@gmail.com>
2024-09-24 18:38:01 +0800

    update rollup contract and bindings

--- bindings/bindings/l2staking.go
+++ bindings/bindings/l2staking.go
@@ -26,6 +26,7 @@ var (
 	_ = common.Big1
 	_ = types.BloomLookup
 	_ = event.NewSubscription
+	_ = abi.ConvertType
 )
 
 // IL2StakingUndelegation is an auto generated low-level Go binding around an user-defined struct.
@@ -170,11 +171,11 @@ func NewL2StakingFilterer(address common.Address, filterer bind.ContractFilterer
 
 // bindL2Staking binds a generic wrapper to an already deployed contract.
 func bindL2Staking(address common.Address, caller bind.ContractCaller, transactor bind.ContractTransactor, filterer bind.ContractFilterer) (*bind.BoundContract, error) {
-	parsed, err := abi.JSON(strings.NewReader(L2StakingABI))
+	parsed, err := L2StakingMetaData.GetAbi()
 	if err != nil {
 		return nil, err
 	}
-	return bind.NewBoundContract(address, parsed, caller, transactor, filterer), nil
+	return bind.NewBoundContract(address, *parsed, caller, transactor, filterer), nil
 }
 
 // Call invokes the (constant) contract method with params as input values and
