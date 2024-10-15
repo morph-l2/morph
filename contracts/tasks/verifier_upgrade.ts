4f35b6cd6f38fb70622d8e2581a87614ed4f899c
chengwenxi <22697326+chengwenxi@users.noreply.github.com>
2024-10-15 20:04:49 +0800

    add verifier upgrade task

---
+++ contracts/tasks/verifier_upgrade.ts
@@ -0,0 +1,44 @@
+import "@nomiclabs/hardhat-web3";
+import "@nomiclabs/hardhat-ethers";
+import "@nomiclabs/hardhat-waffle";
+
+import { task } from "hardhat/config";
+import { ethers } from "ethers";
+import { ImplStorageName, ContractFactoryName } from "../src/types"
+import { storage } from "../src/deploy-utils"
+
+// yarn hardhat upgradeVerifier --version 0 --startBatchIndex 0 --multipleVersionRollupVerifier 0x0165878a594ca255338adfa4d48449f69242eb8f --network l1
+task("upgradeVerifier")
+    .addParam("version")
+    .addParam("startBatchIndex")
+    .addParam("multipleVersionRollupVerifier")
+    .setAction(async (taskArgs, hre) => {
+        const config = hre.deployConfig
+        const newPath = taskArgs.newpath
+
+        // deploy ZkEvmVerifierV1
+        const ZkEvmVerifierV1ContractFactoryName = ContractFactoryName.ZkEvmVerifierV1
+        const ZkEvmVerifierV1ImplStorageName = ImplStorageName.ZkEvmVerifierV1StorageName
+        const Factory = await hre.ethers.getContractFactory(ZkEvmVerifierV1ContractFactoryName)
+        const contract = await Factory.deploy(config.programVkey)
+        await contract.deployed()
+        console.log("%s=%s ; TX_HASH: %s", ZkEvmVerifierV1ImplStorageName, contract.address.toLocaleLowerCase(), contract.deployTransaction.hash);
+        let blockNumber = await hre.ethers.provider.getBlockNumber()
+        console.log("BLOCK_NUMBER: %s", blockNumber)
+        let err = await storage(newPath, ZkEvmVerifierV1ImplStorageName, contract.address.toLocaleLowerCase(), blockNumber || 0)
+        if (err != '') {
+            return err
+        }
+
+
+        // add verifier to MultipleVersionRollupVerifier
+        const MultipleVersionRollupVerifierFactoryName = ContractFactoryName.MultipleVersionRollupVerifier
+        const MultipleVersionRollupVerifierFactory = await hre.ethers.getContractFactory(MultipleVersionRollupVerifierFactoryName)
+        const MultipleVersionRollupVerifier = MultipleVersionRollupVerifierFactory.attach(taskArgs.multipleVersionRollupVerifier)
+
+        const res = await MultipleVersionRollupVerifier.updateVerifier(taskArgs.version, taskArgs.startBatchIndex, contract.address.toLocaleLowerCase())
+
+        const receipt = await res.wait()
+        console.log(`receipt status : ${receipt.status}`)
+        console.log("upgrade verifier successfully, verifier: %s, version: %s, startBatchIndex: %s", contract.address.toLocaleLowerCase(), taskArgs.version, taskArgs.startBatchIndex)
+    });
