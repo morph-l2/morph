b4a417c8a804cdfefd345e577121ba8030ce4962
anylots <22675649+anylots@users.noreply.github.com>
2024-11-14 23:33:25 +0800

    change syscall

--- prover/bin/host/src/main.rs
+++ prover/bin/host/src/main.rs
@@ -2,14 +2,14 @@ use clap::Parser;
 use morph_prover::prove;
 
 /// The arguments for the command.
-#[derive(Parser, Debug)]
-#[clap(author, version, about, long_about = None)]
-struct Args {
-    #[clap(long)]
-    prove: bool,
-}
+// #[derive(Parser, Debug)]
+// #[clap(author, version, about, long_about = None)]
+// struct Args {
+//     #[clap(long)]
+//     prove: bool,
+// }
 
 fn main() {
-    let args = Args::parse();
-    prove("../../testdata/devnet_batch_traces.json", args.prove);
+    // let args = Args::parse();
+    prove("../../testdata/devnet_batch_traces.json", true);
 }
