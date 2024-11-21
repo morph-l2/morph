c34ea51b2a5da0ddbc18b47a94f59cec036db700
WorldDogs <33647825+WorldDogs@users.noreply.github.com>
2024-11-21 16:46:58 +0800

    feat(tx-submitter): add rollup loss control functionality
    - Implement batch cache to store and retrieve rollup batches
    - Add logic to calculate and compare transaction fees with collected L1 fees
    - Introduce new configuration options for rollup loss control
    - Update transaction submission process to include fee checks
    - Add unit tests for new functionality

--- tx-submitter/entry.go
+++ tx-submitter/entry.go
@@ -69,6 +69,9 @@ func Main() func(ctx *cli.Context) error {
 			"rough_estimate_per_l1_msg", cfg.RollupTxGasPerL1Msg,
 			"log_level", cfg.LogLevel,
 			"leveldb_pathname", cfg.LeveldbPathName,
+			"l1_stop_in_blocks", cfg.BlockNotIncreasedThreshold,
+			"rollup_loss_control", cfg.RollupLossControl,
+			"rolluo_loss_buffer", cfg.MaxRollupLossBuffer,
 		)
 
 		ctx, cancel := context.WithCancel(context.Background())
