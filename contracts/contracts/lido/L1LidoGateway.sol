4840d059c162de94004816c470422b8c8a2fe1fd
kukoomomo <kukoomomo@gmail.com>
2024-12-30 16:13:32 +0800

    replace require to revert

--- contracts/contracts/lido/L1LidoGateway.sol
+++ contracts/contracts/lido/L1LidoGateway.sol
@@ -39,8 +39,9 @@ contract L1LidoGateway is L1ERC20Gateway, LidoBridgeableTokens, LidoGatewayManag
     /// @param _l1Token The address of the bridged token in the L1 chain
     /// @param _l2Token The address of the token minted on the L2 chain when token bridged
     constructor(address _l1Token, address _l2Token) LidoBridgeableTokens(_l1Token, _l2Token) {
-        require(_l1Token != address(0), "zero l1token address");
-        require(_l2Token != address(0), "zero l2Token address");
+        if (_l1Token == address(0) || _l2Token ==address(0)){
+              revert ErrorZeroAddress();
+        }
 
         _disableInitializers();
     }
