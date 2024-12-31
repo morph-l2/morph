d83906b6807a87318e37d85f05adfc747ec0d124
arvin.sun <arvin.sun@bitget.com>
2024-12-31 11:36:59 +0800

    update makefile and update deploy config

--- MakefileEks.mk
+++ MakefileEks.mk
@@ -18,17 +18,6 @@ build-bk-prod-morph-prod-mainnet-to-morph-gas-price-oracle:
 start-bk-prod-morph-prod-mainnet-to-morph-gas-price-oracle:
 	/data/secret-manager-wrapper ./app
 
-# hoelsky
-build-bk-prod-morph-prod-testnet-to-morph-gas-price-oracle-holeksy:
-	if [ ! -d dist ]; then mkdir -p dist; fi
-	cd $(PWD)/gas-oracle/app && cargo build --release
-	cp gas-oracle/app/target/release/app dist/
-	aws s3 cp s3://morph-0582-morph-technical-department-testnet-data/testnet/holesky/morph-setup/secret-manager-wrapper.tar.gz ./
-	tar -xvzf secret-manager-wrapper.tar.gz
-
-
-start-bk-prod-morph-prod-testnet-to-morph-gas-price-oracle-holesky:
-	/data/secret-manager-wrapper ./app
 
 # qanet
 build-bk-test-morph-test-qanet-to-morph-gas-price-oracle-qanet:
@@ -56,18 +45,6 @@ build-bk-prod-morph-prod-mainnet-to-morph-prover:
 start-bk-prod-morph-prod-mainnet-to-morph-prover:
 	/data/secret-manager-wrapper ./prover-server
 
-# holesky
-build-bk-prod-morph-prod-testnet-to-morph-prover-holesky:
-	if [ ! -d dist ]; then mkdir -p dist; fi
-	cd $(PWD)/prover/bin/server && RUSTFLAGS="-C target-feature=+avx2,+avx512f" cargo build --release
-	cp prover/target/release/prover-server dist/
-	cp -r prover/configs dist/
-	aws s3 cp s3://morph-0582-morph-technical-department-testnet-data/testnet/holesky/morph-setup/secret-manager-wrapper.tar.gz ./
-	tar -xvzf secret-manager-wrapper.tar.gz
-
-start-bk-prod-morph-prod-testnet-to-morph-prover-holeksy:
-	/data/secret-manager-wrapper ./prover-server
-
 # challenge-handler
 # mainnet
 build-bk-prod-morph-prod-mainnet-to-morph-challenge-handler:
@@ -79,18 +56,6 @@ build-bk-prod-morph-prod-mainnet-to-morph-challenge-handler:
 
 
 start-bk-prod-morph-prod-mainnet-to-morph-challenge-handler:
-	/data/secret-manager-wrapper ./challenge-handler
-
-# holesky
-build-bk-prod-morph-prod-testnet-to-morph-challenge-handler-holesky:
-	if [ ! -d dist ]; then mkdir -p dist; fi
-	cd $(PWD)/prover/bin/challenge && cargo build --release
-	cp prover/bin/challenge/target/release/challenge-handler dist/
-	aws s3 cp s3://morph-0582-morph-technical-department-testnet-data/testnet/holesky/morph-setup/secret-manager-wrapper.tar.gz ./
-	tar -xvzf secret-manager-wrapper.tar.gz
-
-
-start-bk-prod-morph-prod-testnet-to-morph-challenge-handler-holesky:
 	/data/secret-manager-wrapper ./challenge-handler
 
 # shadow-proving
@@ -105,17 +70,6 @@ build-bk-prod-morph-prod-mainnet-to-morph-shadow-proving:
 start-bk-prod-morph-prod-mainnet-to-morph-shadow-proving:
 	/data/secret-manager-wrapper  ./shadow-proving
 
-# holesky
-build-bk-prod-morph-prod-testnet-to-morph-shadow-proving-holesky:
-	if [ ! -d dist ]; then mkdir -p dist; fi
-	cd $(PWD)/prover/bin/shadow-prove && cargo build --release
-	cp prover/bin/shadow-prove/target/release/shadow-proving dist/
-	aws s3 cp s3://morph-0582-morph-technical-department-testnet-data/testnet/holesky/morph-setup/secret-manager-wrapper.tar.gz ./
-	tar -xvzf secret-manager-wrapper.tar.gz
-
-start-bk-prod-morph-prod-testnet-to-morph-shadow-proving-holesky:
-	/data/secret-manager-wrapper  ./shadow-proving
-
 # staking-oracle
 # mainnet
 build-bk-prod-morph-prod-mainnet-to-morph-staking-oracle:
@@ -126,17 +80,6 @@ build-bk-prod-morph-prod-mainnet-to-morph-staking-oracle:
 	tar -xvzf secret-manager-wrapper.tar.gz
 
 start-bk-prod-morph-prod-mainnet-to-morph-staking-oracle:
-	/data/secret-manager-wrapper  ./staking-oracle
-
-# holesky
-build-bk-prod-morph-prod-testnet-to-morph-staking-oracle-holesky:
-	if [ ! -d dist ]; then mkdir -p dist; fi
-	env GO111MODULE=on CGO_LDFLAGS="-ldl" CGO_ENABLED=1 go build -v $(LDFLAGS) -o oracle/staking-oracle ./oracle/cmd/staking-oracle
-	cp oracle/staking-oracle dist/
-	aws s3 cp s3://morph-0582-morph-technical-department-testnet-data/testnet/holesky/morph-setup/secret-manager-wrapper.tar.gz ./
-	tar -xvzf secret-manager-wrapper.tar.gz
-
-start-bk-prod-morph-prod-testnet-to-morph-staking-oracle-holesky:
 	/data/secret-manager-wrapper  ./staking-oracle
 
 # qanet
