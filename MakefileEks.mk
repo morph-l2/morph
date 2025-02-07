7fce58981d888ed515fb52d5d750b201aa4a16d0
Kathy <22675649+anylots@users.noreply.github.com>
2025-02-07 11:29:17 +0800

    add testnet deploy config (#716)

--- MakefileEks.mk
+++ MakefileEks.mk
@@ -92,3 +92,67 @@ build-bk-test-morph-test-qanet-to-morph-staking-oracle-qanet:
 
 start-bk-test-morph-test-qanet-to-morph-staking-oracle-qanet:
 	/data/secret-manager-wrapper  ./staking-oracle
+
+
+# gas-oracle
+# testnet
+build-bk-prod-morph-prod-testnet-to-morph-gas-price-oracle-holesky:
+	if [ ! -d dist ]; then mkdir -p dist; fi
+	cd $(PWD)/gas-oracle/app && cargo build --release
+	cp gas-oracle/app/target/release/app dist/
+	aws s3 cp s3://morph-0582-morph-technical-department-testnet-data/testnet/holesky/morph-setup/secret-manager-wrapper.tar.gz ./
+	tar -xvzf secret-manager-wrapper.tar.gz
+
+
+start-bk-prod-morph-prod-testnet-to-morph-gas-price-oracle-holesky:
+	/data/secret-manager-wrapper ./app
+
+# prover
+# testnet
+build-bk-prod-morph-prod-testnet-to-morph-prover-holesky:
+	if [ ! -d dist ]; then mkdir -p dist; fi
+	cd $(PWD)/prover/bin/server && RUSTFLAGS="-C target-feature=+avx2,+avx512f" cargo build --release
+	cp prover/target/release/prover-server dist/
+	cp -r prover/configs dist/
+	aws s3 cp s3://morph-0582-morph-technical-department-testnet-data/testnet/holesky/morph-setup/secret-manager-wrapper.tar.gz ./
+	tar -xvzf secret-manager-wrapper.tar.gz
+
+start-bk-prod-morph-prod-testnet-to-morph-prover-holesky:
+	/data/secret-manager-wrapper ./prover-server
+
+# challenge-handler
+# testnet
+build-bk-prod-morph-prod-testnet-to-morph-challenge-handler-holesky:
+	if [ ! -d dist ]; then mkdir -p dist; fi
+	cd $(PWD)/prover/bin/challenge && cargo build --release
+	cp prover/bin/challenge/target/release/challenge-handler dist/
+	aws s3 cp s3://morph-0582-morph-technical-department-testnet-data/testnet/holesky/morph-setup/secret-manager-wrapper.tar.gz ./
+	tar -xvzf secret-manager-wrapper.tar.gz
+
+
+start-bk-prod-morph-prod-testnet-to-morph-challenge-handler-holesky:
+	/data/secret-manager-wrapper ./challenge-handler
+
+# shadow-proving
+# testnet
+build-bk-prod-morph-prod-testnet-to-morph-shadow-proving-holesky:
+	if [ ! -d dist ]; then mkdir -p dist; fi
+	cd $(PWD)/prover/bin/shadow-prove && cargo build --release
+	cp prover/bin/shadow-prove/target/release/shadow-proving dist/
+	aws s3 cp s3://morph-0582-morph-technical-department-testnet-data/testnet/holesky/morph-setup/secret-manager-wrapper.tar.gz ./
+	tar -xvzf secret-manager-wrapper.tar.gz
+
+start-bk-prod-morph-prod-testnet-to-morph-shadow-proving-holesky:
+	/data/secret-manager-wrapper  ./shadow-proving
+
+# staking-oracle
+# testnet
+build-bk-prod-morph-prod-testnet-to-morph-staking-oracle-holesky:
+	if [ ! -d dist ]; then mkdir -p dist; fi
+	env GO111MODULE=on CGO_LDFLAGS="-ldl" CGO_ENABLED=1 go build -v $(LDFLAGS) -o oracle/staking-oracle ./oracle/cmd/staking-oracle
+	cp oracle/staking-oracle dist/
+	aws s3 cp s3://morph-0582-morph-technical-department-testnet-data/testnet/holesky/morph-setup/secret-manager-wrapper.tar.gz ./
+	tar -xvzf secret-manager-wrapper.tar.gz
+
+start-bk-prod-morph-prod-testnet-to-morph-staking-oracle-holesky:
+	/data/secret-manager-wrapper  ./staking-oracle
