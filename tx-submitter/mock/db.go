8d5626496e60017ad562aec0ad7a8653ceea6d84
WorldDogs <noreply.github.com>
2025-03-19 15:43:51 +0800

    chore: remove unnecessary header comments from db.go
    - Deleted the file header comments in db.go to streamline the code and improve readability.

--- tx-submitter/mock/db.go
+++ tx-submitter/mock/db.go
@@ -1,11 +1,3 @@
-/*
- * @Author: WorldDogs noreply.github.com
- * @Date: 2025-03-12 13:00:15
- * @LastEditors: WorldDogs noreply.github.com
- * @LastEditTime: 2025-03-12 13:04:57
- * @FilePath: /morph/tx-submitter/mock/db.go
- * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
- */
 package mock
 
 import (
