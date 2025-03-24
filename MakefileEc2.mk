3708940d748e2bc060c4b1341bd16d390de03846
arvin.sun <arvin.sun@bitget.com>
2025-03-24 18:15:45 +0800

    update makefileeks

--- MakefileEc2.mk
+++ MakefileEc2.mk
@@ -37,3 +37,19 @@ build-bk-prod-morph-prod-testnet-to-morph-tx-submitter-holesky:
 	cp tx-submitter/tx-submitter dist/
 	tar -czvf tx-submitter.tar.gz dist
 	aws s3 cp tx-submitter.tar.gz s3://morph-0582-morph-technical-department-testnet-data/testnet/holesky/morph-setup/tx-submitter.tar.gz
+
+
+build-bk-test-morph-test-qanet-to-morph-node-qanet:
+	if [ ! -d dist ]; then mkdir -p dist; fi
+	cd $(PWD)/node && make build
+	cp node/build/bin/morphnode dist/
+	cp node/build/bin/tendermint dist/
+	tar -czvf morph-node.tar.gz dist
+	aws s3 cp morph-node.tar.gz s3://morph-7637-morph-technical-department-qanet-data/morph-setup/morph-node.tar.gz
+
+build-bk-test-morph-test-qanet-to-morph-tx-submitter-qanet:
+	if [ ! -d dist ]; then mkdir -p dist; fi
+	env GO111MODULE=on CGO_LDFLAGS="-ldl" CGO_ENABLED=1 go build -v $(LDFLAGS) -o tx-submitter/tx-submitter ./tx-submitter/cmd
+	cp tx-submitter/tx-submitter dist/
+	tar -czvf tx-submitter.tar.gz dist
+	aws s3 cp tx-submitter.tar.gz s3://morph-7637-morph-technical-department-qanet-data/morph-setup/tx-submitter.tar.gz
