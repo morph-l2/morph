78f17e0ea0a54b66856956620842e32263b6dfbc
Segue <huoda.china@163.com>
2025-07-30 17:06:10 +0800

    Add morph token (#768)
    Co-authored-by: kukoomomo <kukoomomo@gmail.com>

---
+++ contracts/contracts/l2/token/IMorphPlacementToken.sol
@@ -0,0 +1,31 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity =0.8.24;
+
+import {IERC20MetadataUpgradeable} from "@openzeppelin/contracts-upgradeable/token/ERC20/extensions/IERC20MetadataUpgradeable.sol";
+
+/**
+ /// @dev Interface of the MorphToken standard as defined in the EIP.
+ */
+interface IMorphPlacementToken is IERC20MetadataUpgradeable {
+    /// @dev Atomically increases the allowance granted to `spender` by the caller.
+    ///
+    /// This is an alternative to {approve} that can be used as a mitigation for
+    /// problems described in {IMorphToken-approve}.
+    ///
+    /// Requirements:
+    ///
+    /// - `spender` cannot be the zero address.
+    function increaseAllowance(address spender, uint256 addedValue) external returns (bool);
+
+    /// @dev Atomically decreases the allowance granted to `spender` by the caller.
+    ///
+    /// This is an alternative to {approve} that can be used as a mitigation for
+    /// problems described in {IMorphToken-approve}.
+    ///
+    /// Requirements:
+    ///
+    /// - `spender` cannot be the zero address.
+    /// - `spender` must have allowance for the caller of at least `subtractedValue`.
+    function decreaseAllowance(address spender, uint256 subtractedValue) external returns (bool);
+}
