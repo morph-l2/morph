80978514f827902f918e120f8b327163ad4faa28
kukoo <kukoomomo@gmail.com>
2025-08-28 18:38:27 +0800

    Config rm (#775)

--- contracts/scripts/localDeploy.sh
+++ contracts/scripts/localDeploy.sh
@@ -1,5 +1,5 @@
 rm -rf ./deployFile.json &&
-    yarn hardhat deploy --storagepath ./deployFile.json --network local &&
-    yarn hardhat initialize --storagepath ./deployFile.json --network local &&
-    yarn hardhat fund --network local &&
-    yarn hardhat register --storagepath ./deployFile.json --network local
+    yarn hardhat deploy --storagepath ./deployFile.json --network l1 &&
+    yarn hardhat initialize --storagepath ./deployFile.json --network l1 &&
+    yarn hardhat fund --network l1 &&
+    yarn hardhat register --storagepath ./deployFile.json --network l1
