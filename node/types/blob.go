faa8c82a0e07a8f53dee0cfc3c355c27e6f96116
fletcher.fan <fletcher.fan@bitget.com>
2025-11-04 17:14:18 +0800

    remove blob proof generation when making blob

--- node/types/blob.go
+++ node/types/blob.go
@@ -52,17 +52,13 @@ func RetrieveBlobBytes(blob *kzg4844.Blob) ([]byte, error) {
 	return data, nil
 }
 
-func makeBCP(bz []byte) (b kzg4844.Blob, c kzg4844.Commitment, p kzg4844.Proof, err error) {
+func makeBlobCommitment(bz []byte) (b kzg4844.Blob, c kzg4844.Commitment, err error) {
 	blob, err := MakeBlobCanonical(bz)
 	if err != nil {
 		return
 	}
 	b = *blob
 	c, err = kzg4844.BlobToCommitment(&b)
-	if err != nil {
-		return
-	}
-	p, err = kzg4844.ComputeBlobProof(&b, c)
 	if err != nil {
 		return
 	}
@@ -85,20 +81,19 @@ func MakeBlobTxSidecar(blobBytes []byte) (*eth.BlobTxSidecar, error) {
 		err         error
 		blobs       = make([]kzg4844.Blob, blobCount)
 		commitments = make([]kzg4844.Commitment, blobCount)
-		proofs      = make([]kzg4844.Proof, blobCount)
 	)
 	switch blobCount {
 	case 1:
-		blobs[0], commitments[0], proofs[0], err = makeBCP(blobBytes)
+		blobs[0], commitments[0], err = makeBlobCommitment(blobBytes)
 		if err != nil {
 			return nil, err
 		}
 	case 2:
-		blobs[0], commitments[0], proofs[0], err = makeBCP(blobBytes[:MaxBlobBytesSize])
+		blobs[0], commitments[0], err = makeBlobCommitment(blobBytes[:MaxBlobBytesSize])
 		if err != nil {
 			return nil, err
 		}
-		blobs[1], commitments[1], proofs[1], err = makeBCP(blobBytes[MaxBlobBytesSize:])
+		blobs[1], commitments[1], err = makeBlobCommitment(blobBytes[MaxBlobBytesSize:])
 		if err != nil {
 			return nil, err
 		}
@@ -106,7 +101,6 @@ func MakeBlobTxSidecar(blobBytes []byte) (*eth.BlobTxSidecar, error) {
 	return &eth.BlobTxSidecar{
 		Blobs:       blobs,
 		Commitments: commitments,
-		Proofs:      proofs,
 	}, nil
 }
 
