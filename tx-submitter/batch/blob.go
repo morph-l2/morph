a195d1fbf09025073bfa14b4067de758dcd2b728
kukoo <kukoomomo@gmail.com>
2026-03-04 10:47:10 +0800

    Test submitter batch (#896)

---
+++ tx-submitter/batch/blob.go
@@ -0,0 +1,210 @@
+package batch
+
+import (
+	"bytes"
+	"encoding/binary"
+	"errors"
+	"fmt"
+	"io"
+	"morph-l2/node/zstd"
+
+	eth "github.com/morph-l2/go-ethereum/core/types"
+	"github.com/morph-l2/go-ethereum/crypto/kzg4844"
+	"github.com/morph-l2/go-ethereum/rlp"
+)
+
+const MaxBlobBytesSize = 4096 * 31
+
+var (
+	emptyBlob          = new(kzg4844.Blob)
+	emptyBlobCommit, _ = kzg4844.BlobToCommitment(emptyBlob)
+	emptyBlobProof, _  = kzg4844.ComputeBlobProof(emptyBlob, emptyBlobCommit)
+)
+
+// MakeBlobCanonical converts the raw blob data into the canonical blob representation of 4096 BLSFieldElements.
+func MakeBlobCanonical(blobBytes []byte) (b *kzg4844.Blob, err error) {
+	if len(blobBytes) > MaxBlobBytesSize {
+		return nil, fmt.Errorf("data is too large for blob. len=%v", len(blobBytes))
+	}
+	offset := 0
+	b = new(kzg4844.Blob)
+	// encode (up to) 31 bytes of remaining input data at a time into the subsequent field element
+	for i := 0; i < 4096; i++ {
+		offset += copy(b[i*32+1:i*32+32], blobBytes[offset:])
+		if offset == len(blobBytes) {
+			break
+		}
+	}
+	if offset < len(blobBytes) {
+		return nil, fmt.Errorf("failed to fit all data into blob. bytes remaining: %v", len(blobBytes)-offset)
+	}
+	return
+}
+
+func RetrieveBlobBytes(blob *kzg4844.Blob) ([]byte, error) {
+	data := make([]byte, MaxBlobBytesSize)
+	for i := 0; i < 4096; i++ {
+		if blob[i*32] != 0 {
+			return nil, fmt.Errorf("invalid blob, found non-zero high order byte %x of field element %d", data[i*32], i)
+		}
+		copy(data[i*31:i*31+31], blob[i*32+1:i*32+32])
+	}
+	return data, nil
+}
+
+func makeBlobCommitment(bz []byte) (b kzg4844.Blob, c kzg4844.Commitment, err error) {
+	blob, err := MakeBlobCanonical(bz)
+	if err != nil {
+		return
+	}
+	b = *blob
+	c, err = kzg4844.BlobToCommitment(&b)
+	if err != nil {
+		return
+	}
+	return
+}
+
+func MakeBlobTxSidecar(blobBytes []byte) (*eth.BlobTxSidecar, error) {
+	if len(blobBytes) == 0 {
+		return &eth.BlobTxSidecar{
+			Blobs:       []kzg4844.Blob{*emptyBlob},
+			Commitments: []kzg4844.Commitment{emptyBlobCommit},
+			Proofs:      []kzg4844.Proof{emptyBlobProof},
+		}, nil
+	}
+	if len(blobBytes) > 2*MaxBlobBytesSize {
+		return nil, errors.New("only 2 blobs at most is allowed")
+	}
+	blobCount := len(blobBytes)/(MaxBlobBytesSize+1) + 1
+	var (
+		err         error
+		blobs       = make([]kzg4844.Blob, blobCount)
+		commitments = make([]kzg4844.Commitment, blobCount)
+	)
+	switch blobCount {
+	case 1:
+		blobs[0], commitments[0], err = makeBlobCommitment(blobBytes)
+		if err != nil {
+			return nil, err
+		}
+	case 2:
+		blobs[0], commitments[0], err = makeBlobCommitment(blobBytes[:MaxBlobBytesSize])
+		if err != nil {
+			return nil, err
+		}
+		blobs[1], commitments[1], err = makeBlobCommitment(blobBytes[MaxBlobBytesSize:])
+		if err != nil {
+			return nil, err
+		}
+	}
+	return &eth.BlobTxSidecar{
+		Blobs:       blobs,
+		Commitments: commitments,
+	}, nil
+}
+
+func CompressBatchBytes(batchBytes []byte) ([]byte, error) {
+	if len(batchBytes) == 0 {
+		return nil, nil
+	}
+	compressedBatchBytes, err := zstd.CompressBatchBytes(batchBytes)
+	if err != nil {
+		return nil, fmt.Errorf("failed to compress batch bytes, err: %w", err)
+	}
+	return compressedBatchBytes, nil
+}
+
+func DecodeTxsFromBytes(txsBytes []byte) (eth.Transactions, error) {
+	reader := bytes.NewReader(txsBytes)
+	txs := make(eth.Transactions, 0)
+	for {
+		var (
+			firstByte   byte
+			fullTxBytes []byte
+			innerTx     eth.TxData
+			err         error
+		)
+		if err = binary.Read(reader, binary.BigEndian, &firstByte); err != nil {
+			// if the blob byte array is completely consumed, then break the loop
+			if err == io.EOF {
+				break
+			}
+			return nil, err
+		}
+		// zero byte is found after valid tx bytes, break the loop
+		if firstByte == 0 {
+			break
+		}
+
+		switch firstByte {
+		case eth.AccessListTxType:
+			if err := binary.Read(reader, binary.BigEndian, &firstByte); err != nil {
+				return nil, err
+			}
+			innerTx = new(eth.AccessListTx)
+		case eth.DynamicFeeTxType:
+			if err := binary.Read(reader, binary.BigEndian, &firstByte); err != nil {
+				return nil, err
+			}
+			innerTx = new(eth.DynamicFeeTx)
+		case eth.SetCodeTxType:
+			if err := binary.Read(reader, binary.BigEndian, &firstByte); err != nil {
+				return nil, err
+			}
+			innerTx = new(eth.SetCodeTx)
+		case eth.AltFeeTxType:
+			if err := binary.Read(reader, binary.BigEndian, &firstByte); err != nil {
+				return nil, err
+			}
+			innerTx = new(eth.AltFeeTx)
+		default:
+			if firstByte <= 0xf7 { // legacy tx first byte must be greater than 0xf7(247)
+				return nil, fmt.Errorf("not supported tx type: %d", firstByte)
+			}
+			innerTx = new(eth.LegacyTx)
+		}
+
+		// we support the tx types of LegacyTxType/AccessListTxType/DynamicFeeTxType
+		//if firstByte == eth.AccessListTxType || firstByte == eth.DynamicFeeTxType {
+		//	// the firstByte here is used to indicate tx type, so skip it
+		//	if err := binary.Read(reader, binary.BigEndian, &firstByte); err != nil {
+		//		return nil, err
+		//	}
+		//} else if firstByte <= 0xf7 { // legacy tx first byte must be greater than 0xf7(247)
+		//	return nil, fmt.Errorf("not supported tx type: %d", firstByte)
+		//}
+		fullTxBytes, err = extractInnerTxFullBytes(firstByte, reader)
+		if err != nil {
+			return nil, err
+		}
+		if err = rlp.DecodeBytes(fullTxBytes, innerTx); err != nil {
+			return nil, err
+		}
+		txs = append(txs, eth.NewTx(innerTx))
+	}
+	return txs, nil
+}
+
+func extractInnerTxFullBytes(firstByte byte, reader io.Reader) ([]byte, error) {
+	//the occupied byte length for storing the size of the following rlp encoded bytes
+	sizeByteLen := firstByte - 0xf7
+
+	// the size of the following rlp encoded bytes
+	sizeByte := make([]byte, sizeByteLen)
+	if err := binary.Read(reader, binary.BigEndian, sizeByte); err != nil {
+		return nil, err
+	}
+	size := binary.BigEndian.Uint32(append(make([]byte, 4-len(sizeByte)), sizeByte...))
+
+	txRaw := make([]byte, size)
+	if err := binary.Read(reader, binary.BigEndian, txRaw); err != nil {
+		return nil, err
+	}
+	fullTxBytes := make([]byte, 1+uint32(sizeByteLen)+size)
+	copy(fullTxBytes[:1], []byte{firstByte})
+	copy(fullTxBytes[1:1+sizeByteLen], sizeByte)
+	copy(fullTxBytes[1+sizeByteLen:], txRaw)
+
+	return fullTxBytes, nil
+}
