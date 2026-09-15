528967113f429ad72a1944d468a6092955c25053
marvel.yu <marvelfisher@gmail.com>
2026-09-15 17:40:27 +0800

    use make morphnode-repro

--- MakefileEc2.mk
+++ MakefileEc2.mk
@@ -9,10 +9,9 @@ LDFLAGS := -ldflags "$(LDFLAGSSTRING)"
 
 build-bk-prod-morph-prod-mainnet-to-morph-node:
 	if [ ! -d dist ]; then mkdir -p dist; fi
-	cd $(PWD)/node && make build
+	cd $(PWD)/node && make morphnode-repro
 	cp node/build/bin/morphnode dist/
 	sha256sum dist/morphnode
-	cp node/build/bin/tendermint dist/
 	tar -czvf morph-node.tar.gz dist
 	aws s3 cp morph-node.tar.gz s3://morph-0582-morph-technical-department-mainnet-data/morph-setup/morph-node.tar.gz
 
