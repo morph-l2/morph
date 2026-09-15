3cd3795012747d4ff682d2782168fe3cf83aefb1
marvel.yu <marvelfisher@gmail.com>
2026-09-15 17:23:05 +0800

    add print sha256

--- MakefileEc2.mk
+++ MakefileEc2.mk
@@ -11,6 +11,7 @@ build-bk-prod-morph-prod-mainnet-to-morph-node:
 	if [ ! -d dist ]; then mkdir -p dist; fi
 	cd $(PWD)/node && make build
 	cp node/build/bin/morphnode dist/
+	sha256sum dist/morphnode
 	cp node/build/bin/tendermint dist/
 	tar -czvf morph-node.tar.gz dist
 	aws s3 cp morph-node.tar.gz s3://morph-0582-morph-technical-department-mainnet-data/morph-setup/morph-node.tar.gz
